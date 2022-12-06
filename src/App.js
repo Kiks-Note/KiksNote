b5b412eef1df89bb4d1f3f64f27f734e2ff254fd
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-12-06 15:01:03 +0100

    add index.js view all pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import './components/blog/calendar/Calendar.scss';
-import Calendar from './components/blog/calendar/Calendar.jsx'
-
+// import Calendar from './components/blog/calendar/Calendar.jsx'
+import { Register } from './pages'
 
 function App() {
   return (
     <div className="App">
-
-      <Calendar/>   
+      <Register/>
+      {/* <Calendar/> */}
        </div>
-
-    
-    
-
-
-
-
-
   );
 }
 
