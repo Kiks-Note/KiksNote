48ba99842b54460cf0af326e0aeb284634c343d0
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-12-06 17:03:02 +0100

    add validation form

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,118 +1,214 @@
 import React, { useState } from 'react';
 
-const initialState = {
-    lastName: '',
-    firstName: '',
-    password: '',
-    confirmPassword: '',
-    birthdate: '',
-    status: '',
-    mail: '',
-}
-
 const Register = () => {
-    const [form, setForm] = useState(initialState);
     const [isSignup, setIsSignup] = useState(true);
-
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
+    const [form, setForm] = useState({
+        firstName: '',
+        lastName: '',
+        password: '',
+        confirmPassword: '',
+        email: '',
+        birthdate: '',
+        status: ''
+    });
+     
+    const [error, setError] = useState({
+        firstName: '',
+        lastName: '',
+        password: '',
+        confirmPassword: '',
+        email: '',
+        birthdate: '',
+        status: ''
+    })
+     
+    const onInputChange = e => {
+        const { name, value } = e.target;
+        setForm(prev => ({
+          ...prev,
+          [name]: value
+    }));
+        validateForm(e);
     }
+     
+    const validateForm = e => {
+        let { name, value } = e.target;
+        setError(prev => {
+            const stateObj = { ...prev, [name]: "" };
+     
+        switch (name) {
+            case "firstname":
+                if (!value) {
+                    stateObj[name] = "Veuillez entrer un votre nom";
+                }
+            break;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(form);
+            case "lastname":
+                if (!value) {
+                    stateObj[name] = "Veuillez entrer votre prénom";
+                }
+            break;
+     
+            case "password":
+                if (!value) {
+                    stateObj[name] = "Veuillez entrer un mot de passe";
+                } else if (form.confirmPassword && value !== form.confirmPassword) {
+                    stateObj["confirmPassword"] = "Le mot de passe ne correspond pas";
+                } else {
+                    stateObj["confirmPassword"] = form.confirmPassword ? "" : error.confirmPassword;
+                }
+            break;
+     
+            case "confirmPassword":
+                if (!value) {
+                    stateObj[name] = "Confirmez le mot de passe ";
+                } else if (form.password && value !== form.password) {
+                    stateObj[name] = "Le mot de passe ne correspond pas";
+                }
+            break;
 
-        window.location.reload();
-    }
+            case "email":
+                if (!value) {
+                    stateObj[name] = "Veuillez entrer un mail";
+                } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)) {
+                    stateObj[name] = "Adresse mail invalide";
+                }
+                // } else if(form.status == "etudiant" && value != /^[^\s@]+@edu\.itescia\.fr$/) {
+                //     stateObj[name] = "Adresse mail étudiant invalide";
+                // }
+            break;
+
+            case "birthdate":
+                if (!value) {
+                    stateObj[name] = "Veuillez entrer une date de naissance";
+                }
+            break;
+
+            case "status":
+                if (!value) {
+                    stateObj[name] = "Veuillez choisir votre status";
+                } else if(value === "etudiant" && form.email != /^[^\s@]+@edu\.itescia\.fr$/) {
+                    stateObj.email = "Adresse mail étudiant invalide";
+                }
+            break;
+     
+            default:
+              break;
+          }
+     
+          return stateObj;
+        });
+      }
 
     const switchMode = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup);
     }
+
+    // @edu.itescia.fr
 
     return(
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     <p>{isSignup ? 'Inscription' : 'Connexion'}</p>
-                    <form onSubmit={handleSubmit}>
+                    {/* <form onSubmit={handleSubmit}> */}
+                    <form>
                     {isSignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="lastName"></label>
                             <input 
-                                name="lastName" 
                                 type="text"
+                                name="lastname" 
                                 placeholder="Prénom"
-                                onChange={handleChange}
+                                value={form.lastname}
+                                onChange={onInputChange}
+                                onBlur={validateForm}
                                 required
                             />
+                            {error.lastName && <span className='err'>{error.lastName}</span>}
                         </div>
                     )}
                     {isSignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="firstName"></label>
                             <input 
-                                name="firstName" 
                                 type="text"
+                                name="firstname" 
                                 placeholder="Nom"
-                                onChange={handleChange}
+                                value={form.firstname}
+                                onChange={onInputChange}
+                                onBlur={validateForm}
                                 required
                             />
+                            {error.firstname && <span className='err'>{error.firstname}</span>}
                         </div>
                     )}
                     <div className="auth__form-container_fields-content_input">
                         <label htmlFor="mail"></label>
                             <input 
-                                name="mail" 
                                 type="mail"
+                                name="email" 
                                 placeholder="Mail"
-                                onChange={handleChange}
+                                value={form.email}
+                                onChange={onInputChange}
+                                onBlur={validateForm}
                                 required
                             />
+                            {error.email && <span className='err'>{error.email}</span>}
                     </div>
                     {isSignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="birthdate"></label>
                             <input 
-                                name="birthdate"
                                 type="birthdate"
+                                name="birthdate"
                                 placeholder="Date de Naissance"
-                                onChange={handleChange}
+                                value={form.birthdate}
+                                onChange={onInputChange}
+                                onBlur={validateForm}
                                 required
                             />
+                            {error.birthdate && <span className='err'>{error.birthdate}</span>}
                         </div>
                     )}
                     <div className="auth__form-container_fields-content_input">
                         <label htmlFor="password"></label>
-                        <input 
-                            name="password" 
-                            type="password"
-                            placeholder="Mot de passe"
-                            onChange={handleChange}
-                            required
-                        />
+                            <input 
+                                type="password"
+                                name="password" 
+                                placeholder="Mot de passe"
+                                onChange={onInputChange}
+                                onBlur={validateForm}
+                                required
+                            />
+                            {error.password && <span className='err'>{error.password}</span>}
                     </div>
                     {isSignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="confirmPassword"></label>
                             <input 
-                                name="confirmPassword" 
                                 type="password"
+                                name="confirmPassword" 
                                 placeholder="Confirm Password"
-                                onChange={handleChange}
+                                onChange={onInputChange}
+                                onBlur={validateForm}
                                 required
                             />
+                            {error.confirmPassword && <span className='err'>{error.confirmPassword}</span>}
                         </div>
                     )}
                     {isSignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="status">
-                                <select name="status" onChange={handleChange} required>
-                                    <option disabled={true} value="">
-                                        --Status--
-                                    </option>
-                                    <option value="administration">Administration</option>
-                                    <option value="po">PO</option>
-                                    <option value="codeur">Codeur</option>
+                                <select name="status" 
+                                        onChange={onInputChange}
+                                        onBlur={validateForm} required>
+                                        <option disabled={true} value="">
+                                            --Status--
+                                        </option>
+                                        <option value="etudiant">Étudiant</option>
+                                        <option value="po">PO</option>
+                                        <option value="pedago">Pedago</option>
                                 </select>
                             </label>
                         </div>
@@ -124,12 +220,12 @@ const Register = () => {
                     <div className="auth__form-container_fields-account">
                         <p>
                             {isSignup
-                             ? "Vous avez déjà un compte. " 
-                             : "Pas encore de compte ? Créer en un "
-                             }
-                             <span onClick={switchMode}>
-                             {isSignup ? 'Se connecter' : 'ici'}
-                             </span>
+                                ? "Vous avez déjà un compte. " 
+                                : "Pas encore de compte ? Créer en un "
+                            }
+                            <span onClick={switchMode}>
+                                {isSignup ? 'Se connecter' : 'ici'}
+                            </span>
                         </p>
                     </div>
                 </div>
