e47a79af8159ebd73b08742509c2b8956d2e5ce0
Adrien140401 <91193459+Adrien140401@users.noreply.github.com>
2022-12-06 16:42:12 +0100

    test: NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import FirebaseContextProvider from "./config/firebase";
 import RoutesProvider from "./Routes";
+import { AppBar } from "@mui/material";
 
 function App() {
   return (
     <FirebaseContextProvider>
       <RoutesProvider />
+      <AppBar />
     </FirebaseContextProvider>
   );
 }
