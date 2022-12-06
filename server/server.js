6699d979fcaf58879958cf30f277bdd0f9a32db3
Jerance <michael.jerance@gmail.com>
2022-12-06 18:49:54 +0100

    feat : Creation template login page

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { db } = require("./firebase");
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.post("/addUser", (req, res) => {
   const data = req.body;
