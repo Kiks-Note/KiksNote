17f1c4915d92e54c8cc6503c061589c50ae55f1d
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-12-07 10:24:55 +0100

    #8 feat/ Adding the first forms for profile page and first commit for branch

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test from "./Pages/Test";
+import Profile from "./Pages/profile/Profile";
 import PrivateRoutes from "./Utils/PrivateRoutes";
 
 function RoutesProvider() {
@@ -11,7 +12,7 @@ function RoutesProvider() {
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/test" element={<Test />} />
-
+        <Route path="/profile" element={<Profile />} />
         <Route element={<PrivateRoutes />}>
           {/* <Route path="/" exact element={<Home />} /> */}
         </Route>
