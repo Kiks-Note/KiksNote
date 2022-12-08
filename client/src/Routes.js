694a692d33b2b4e431804f1b0f15201496af3e37
N0ku <lucielecouturier2@gmail.com>
2022-12-08 16:39:03 +0100

    #20 fix/import and delete olf files with french name

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Appel from "./pages/call/Call";
-import Test from "./pages/Test";
+import Call from "./pages/call/Call";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
+
 function RoutesProvider() {
   return (
     <BrowserRouter>
@@ -11,13 +11,9 @@ function RoutesProvider() {
 
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
-        {/* To Use the Navbar change Test by your page
- <Route path="/test" element={<Navbar element={<Test />} />} /> */}
-        <Route path="/" element={<Navbar element={<Test />} />} />
-        <Route path="/test" element={<Navbar element={<Test />} />} />
 
         <Route element={<PrivateRoutes />}>
-          <Route path="/appel" element={<Appel />} />
+          <Route path="/call" element={<Navbar element={<Call />} />} />
           {/* <Route path="/" exact element={<Home />} /> */}
         </Route>
       </Routes>
