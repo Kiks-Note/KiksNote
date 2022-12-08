0ed3763a673fad61fa7486741058e33341b52ffb
Jules <jules.duarte@edu.esiee-it.fr>
2022-12-08 16:16:59 +0100

    #24 feature/qrcodeback beginning

---
+++ server/call.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { db } = require("./firebase");
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.post("/callAdd", (req, res) => {
+  const itemRef = req.body.name;
+  const space = " ";
+  const newItemRef = itemRef.replace(space, "_");
+
+  db.doc("/inventory/" + newItemRef)
+    .set({
+      id_lesson: req.body.id_lesson,
+      qrcode: req.body.qrcode,
+      student_scan: req.body.student_scan,
+      chats: req.body.chats,
+    })
+    .then(() => {
+      res.send("Item added to inventory");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
