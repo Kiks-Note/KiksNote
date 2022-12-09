ee66d8764fac40a7b7c6c1d247c2c190954ad4d2
IlanP <ilan.petiot@icloud.com>
2022-12-09 10:41:35 +0100

    close #27  fix/change route to match with drower

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,26 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Test from "./pages/Test";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home.js";
+
 function RoutesProvider() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* EXAMPLES */}
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* EXAMPLES */}
 
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* To Use the Navbar change Test by your page
+                {/* <Route path="/login" element={<Login />} /> */}
+                {/* <Route path="/register" element={<Register />} /> */}
+                {/* To Use the Navbar change Test by your page
  <Route path="/test" element={<Navbar element={<Test />} />} /> */}
-        <Route path="/" element={<Navbar element={<Test />} />} />
-        <Route path="/test" element={<Navbar element={<Test />} />} />
+                {/*<Route path="/" element={<Navbar element={<Test/>}/>}/>*/}
+                {/*<Route path="/test" element={<Navbar element={<Test/>}/>}/>*/}
 
-        <Route element={<PrivateRoutes />}>
-          {/* <Route path="/" exact element={<Home />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+
+                {/* Write here route that you don't need to be login*/}
+
+
+                <Route element={<PrivateRoutes/>}>
+                    {/* Write here route that you need to be login*/}
+
+                    <Route path="/" element={<Home/>}/>
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default RoutesProvider;
