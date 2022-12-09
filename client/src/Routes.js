e2c2323cfb3b9c53a8c9415010f39919d8a1a91a
IlanP <ilan.petiot@icloud.com>
2022-12-09 09:46:49 +0100

    #5 feat/add blog route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,26 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Test from "./pages/Test";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
+import Blog from "./pages/blog/Blog";
+
 function RoutesProvider() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* EXAMPLES */}
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* EXAMPLES */}
 
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* To Use the Navbar change Test by your page
- <Route path="/test" element={<Navbar element={<Test />} />} /> */}
-        <Route path="/" element={<Navbar element={<Test />} />} />
-        <Route path="/test" element={<Navbar element={<Test />} />} />
+                {/* <Route path="/login" element={<Login />} /> */}
+                {/* <Route path="/register" element={<Register />} /> */}
+                {/* To Use the Navbar change Test by your page
+                <Route path="/test" element={<Navbar element={<Test />} />} /> */}
+                {/*<Route path="/" element={<Navbar element={<Test />} />} />*/}
+                {/*<Route path="/test" element={<Navbar element={<Test />} />} />*/}
 
-        <Route element={<PrivateRoutes />}>
-          {/* <Route path="/" exact element={<Home />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+
+                {/*<Route path="/blog" element={<Blog />} />*/}
+
+
+                <Route element={<PrivateRoutes/>}>
+                    <Route path="/blog" element={<Blog/>}/>
+                    {/* <Route path="/" exact element={<Home />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default RoutesProvider;
