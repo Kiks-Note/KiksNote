d4030577fd1326aadf8df87cb80556bebb68766b
Elim Florvil <elim.florvil@icloud.com>
2022-12-10 18:29:05 +0100

    #16 style/ display card and table

--- client/src/components/card/CardBoard.js
+++ client/src/components/card/CardBoard.js
@@ -4,7 +4,6 @@ import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 export default function CardBoard({ picture, firstname, sprint_group ,fav }) {
@@ -14,11 +13,7 @@ export default function CardBoard({ picture, firstname, sprint_group ,fav }) {
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={<Avatar src={picture}></Avatar>}
-        action={
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon color="secondary" />
-          </IconButton>
-        }
+        action={fav ? <FavoriteIcon color="secondary" /> : ""}
         title={firstname}
         subheader={sprint_group}
       />
--- client/src/pages/board/Board.js
+++ client/src/pages/board/Board.js
@@ -1,52 +1,25 @@
-// import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TableBoard from "../../components/table/TableBoards";
 import CardBoard from "../../components/card/CardBoard";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 function Board() {
-  const columns = [
-    {
-      field: "sprint_name",
-      headerName: "Nom du sprint",
-      flex: 1,
-    },
-    {
-      field: "sprint_group",
-      headerName: "Groupe du sprint",
-      flex: 1,
-    },
-    {
-      field: "start",
-      headerName: "Date de début",
-      flex: 1,
-    },
-    {
-      field: "end",
-      headerName: "Date de fin",
-      flex: 1,
-    },
-    {
-      field: "favorite",
-      headerName: "Favoris",
-      flex: 1,
-      disableReorder: true,
-    },
-  ];
-  const dataBoard = [
+  let dataBoard = [
     {
       id: 1,
       sprint_name: "Java",
       sprint_group: "Kiks",
-      start: "12/12/2022",
-      end: "29/12/2022",
+      start: "12/11/2022",
+      end: "29/11/2022",
       backlog: "lien",
       favorite: true,
       students: {
         student_id: "uid(student)",
         firstname: "Chris",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 2,
@@ -60,12 +33,12 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 3,
-      sprint_name: "Php",
-      sprint_group: "Kiks2",
+      sprint_name: "JavaScript",
+      sprint_group: "TheVie",
       start: "22/12/2022",
       end: "29/12/2022",
       backlog: "lien",
@@ -74,12 +47,12 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 4,
-      sprint_name: "Php",
-      sprint_group: "Kiks2",
+      sprint_name: "Symfony",
+      sprint_group: "SMS",
       start: "22/12/2022",
       end: "29/12/2022",
       backlog: "lien",
@@ -88,12 +61,12 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 5,
-      sprint_name: "Php",
-      sprint_group: "Kiks2",
+      sprint_name: "Python",
+      sprint_group: "BoaTeam",
       start: "22/12/2022",
       end: "29/12/2022",
       backlog: "lien",
@@ -102,12 +75,12 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 6,
       sprint_name: "Php",
-      sprint_group: "Kiks2",
+      sprint_group: "Farid",
       start: "22/12/2022",
       end: "29/12/2022",
       backlog: "lien",
@@ -116,12 +89,12 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
     {
       id: 7,
       sprint_name: "Php",
-      sprint_group: "Kiks2",
+      sprint_group: "Storm",
       start: "22/12/2022",
       end: "29/12/2022",
       backlog: "lien",
@@ -130,33 +103,64 @@ function Board() {
         student_id: "uid(student)",
         firstname: "Elim",
       },
-      picture: "https://thispersondoesnotexist.com/image",
+      picture: "https://picsum.photos/500/300?random=" + Math.random(),
     },
   ];
-  const favorite = dataBoard.filter((person) => person.favorite === true);
+  const [data] = useState(dataBoard);
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
-          {favorite.map((data, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <CardBoard
-                key={data.uid}
-                picture={data.picture}
-                firstname={data.students.firstname}
-                sprint_group={data.sprint_group}
-                fav={data.favorite}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        <Box>
+          <Typography variant="h6" gutterBottom>
+            Favoris
+          </Typography>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {dataBoard
+              .filter((person) => person.favorite === true )
+              .map((person) => (
+                <Grid item xs={2} sm={4} md={4} key={person.uid}>
+                  <CardBoard
+                    key={person.uid}
+                    picture={person.picture}
+                    firstname={person.students.firstname}
+                    sprint_group={person.sprint_group}
+                    fav={person.favorite}
+                  />
+                </Grid>
+              ))}
+          </Grid>
+        </Box>
+        <Box>
+          <Typography variant="h6" gutterBottom>
+            Board actif
+          </Typography>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {dataBoard
+              .filter((person) => person.end < Date())
+              .map((person) => (
+                <Grid item xs={2} sm={4} md={4} key={person.uid}>
+                  <CardBoard
+                    key={person.uid}
+                    picture={person.picture}
+                    firstname={person.students.firstname}
+                    sprint_group={person.sprint_group}
+                    fav={person.favorite}
+                  />
+                </Grid>
+              ))}
+          </Grid>
+        </Box>
       </Box>
-      <Box sx={{ height: 400, width: 1 }}>
-        <TableBoard rows={dataBoard} columns={columns} />
+      <Box sx={{ width: " 100%", marginTop: 4 }}>
+        <TableBoard rows={data} />
       </Box>
     </>
   );
