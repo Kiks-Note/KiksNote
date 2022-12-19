8c004d6e1d0f9f510a81c94ffada67a04fb18311
IlanP <ilan.petiot@icloud.com>
2022-12-19 19:15:42 +0100

    #33 feat/add request for blog

---
+++ server/blog_back.js
@@ -0,0 +1,56 @@
+module.exports = (app, db) => {
+    // app.get("/blogs", (req, res) => {
+    //     const docs = [];
+    //     const comments = [];
+    //     res.setHeader("Access-Control-Allow-Origin", "*");
+    //     db.collection("blog_tutos").get().then((snapshot) => {
+    //         snapshot.forEach((doc) => {
+    //             docs.push(doc.data());
+    //             res.send(docs);
+    //             doc.ref.collection("comment").get().then((snap) => {
+    //                 snap.forEach((com) => {
+    //                     comments.push(com.data());
+    //                     // res.send(comments);
+    //                     // console.log(comments);
+    //                 })
+    //             }).catch((err) => {
+    //                 console.log(err);
+    //             });
+    //         })
+    //     })
+    // })
+
+
+    // get all blogs
+    app.get("/tutos", async (req, res) => {
+        const snapshot = await db.collection('blog_tutos').get();
+        res.send(snapshot.docs.map(doc => doc.data()));
+        // return snapshot.docs.map(doc => doc.data());
+    });
+
+    //get all blogs id
+    app.get("/tutos/id", async (req, res) => {
+        const snapshot = await db.collection('blog_tutos').get();
+        res.send(snapshot.docs.map(doc => doc.id));
+        // return snapshot.docs.map(doc => doc.data());
+    });
+    
+
+    // get blog by id
+    app.get("/tuto/:id", async (req, res) => {
+        const snapshot = await db.collection('blog_tutos').doc(req.params.id).get();
+        res.send(snapshot.data());
+    });
+
+    // get all comments by blog id
+    app.get("/tuto/:id/comments", async (req, res) => {
+        const snapshot = await db.collection('blog_tutos').doc(req.params.id).collection("comment").get();
+        res.send(snapshot.docs.map(doc => doc.data()));
+    });
+
+    // app.post("/blog/:id/comments", async (req, res) => {
+    //     const snapshot = await db.collection('blog_tutos').doc(req.params.id).collection("comment").add(req.body);
+    //     res.send(snapshot.data());
+    // });
+
+}
