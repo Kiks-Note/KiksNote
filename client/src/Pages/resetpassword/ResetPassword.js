26007045e6a7a69e4506bf330988d38bdf2d7792
Mohamed <mohameddrame907@gmail.com>
2023-03-20 16:37:01 +0100

    #46 feat/ toast added and clear input when user submit

--- client/src/Pages/resetpassword/ResetPassword.js
+++ client/src/Pages/resetpassword/ResetPassword.js
@@ -4,10 +4,29 @@ import Divider from '@mui/material/Divider';
 import axios from 'axios';
 import { Box } from "@mui/material";
 
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+const options = {
+    autoClose: 2000,
+    className: '',
+    position: toast.POSITION.TOP_RIGHT,
+  };
+  
+ export const toastSuccess = message => {
+    toast.success(message, options);
+  }
+
+  export const toastFail = message => {
+    toast.error(message, options);
+  }
 
 function ResetPassword() {
     const form = useRef();
 
+    
     
     const [newUserPassword, setnNewUserPassword] = useState(''); 
     const [confirmedUserPassword, setConfirmedUserPassword] = useState(''); 
@@ -23,12 +42,27 @@ function ResetPassword() {
     };
 
 
+    function handleSubmitPassword(e) {
+        resetUserPassword();
+        e.preventDefault(); 
+        if (newUserPassword == confirmedUserPassword) {
+            console.log("yes");
+            form.current.reset();
+            toastSuccess("Mot de passe modifié")
+        } else {
+            console.log("nope");
+            toastFail("Mot de passe différent");
+        }
+        
+    }
+
+
     return (
         <div className="form-reset-with-mail-container">
             <Box sx={{ boxShadow: 5 }} style={{ width: "50%", padding: "15px" }} className="box-form-reset-with-mail">
                 <h1 style={{ margin: 10, fontSize: "2vw" }}> Modification du mot de passe</h1>
                 <Divider variant="middle" style={{ background: "#000", height: "1px", width: "50%" }} className="divider-form-reset-with-mail" />
-                <form ref={form} onSubmit={resetUserPassword} style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <form ref={form} onSubmit={handleSubmitPassword} style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <label>Nouveau mot de passe</label>
                     <input required id="my-new-pass" onInput={(e) => setnNewUserPassword(e.target.value)} type="password" name="user_new_pass" style={{ backgroundColor: "#fff", width: "80%", marginTop: "1%" }} />
                     <label>Confirmer le mot de passe</label>
@@ -36,6 +70,8 @@ function ResetPassword() {
                     <input type="submit" variant="contained" style={{ backgroundColor: "#fff", color: "black", fontWeight: "bold", textTransform: 'none', margin: 10, marginTop: "3%" }} value="Envoyer" />
                 </form>
             </Box>
+            <ToastContainer></ToastContainer>
+
         </div>
     );
 }
