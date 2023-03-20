9d158cf9fb9df06971dea12c126b3772b6fc1ee5
Celiian <90334525+Celiian@users.noreply.github.com>
2023-03-20 11:26:51 +0100

    feat/fixed routing

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,6 +4,7 @@ import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
 import Dashboard from "./pages/board/Dashboard";
 import Board from "./pages/board/Board";
+import Home from "./pages/home/";
 
 function RoutesProvider() {
   return (
@@ -13,14 +14,13 @@ function RoutesProvider() {
 
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
-        {/* To Use the Navbar change Test by your page
-        <Route path="/test" element={<Navbar element={<Test />} />} /> */}
+        {/* To Use the Navbar change Test by your page*/ <Route path="/test" element={<Navbar element={<Test />} />} />}
         <Route path="/" element={<Navbar element={<Test />} />} />
         <Route element={<PrivateRoutes element />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/board" element={<Board />} />
 
-          {/* <Route path="/" exact element={<Home />} /> */}
+          {<Route path="/" exact element={<Home />} />}
         </Route>
       </Routes>
     </BrowserRouter>
