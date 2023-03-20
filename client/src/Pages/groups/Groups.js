9e1381982071e6d735917fa23d47d0a827723aa2
N0ku <lucielecouturier2@gmail.com>
2023-03-20 09:09:53 +0100

    #20 feat/save code template

--- client/src/Pages/groups/Groups.js
+++ client/src/Pages/groups/Groups.js
@@ -1,90 +1,212 @@
-import "./groups.scss";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
-import { useState } from "react";
+import "./Groups.scss";
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
-const allStudents = [
-  { id: "1", username: "jules" },
-  { id: "2", username: "celian" },
-  { id: "3", username: "lucas" },
-  { id: "4", username: "mohamed" },
-  { id: "5", username: "jerance" },
-  { id: "6", username: "rui" },
+const tasks = [
+  {
+    id: "1",
+    name: "Board EduScrum",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "2",
+    name: "Création de sprint agile",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "3",
+    name: "BurnDown chart",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "4",
+    name: "Ajout du backlog",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "5",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    id: "6",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "7",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "8",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "9",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "10",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "11",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "12",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: "13",
+    name: "Sprint retro",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
 ];
 
-function Groups() {
-  const [students, updateStudents] = useState(allStudents);
+const taskStatus = {
+  requested: {
+    name: "Stories",
+    items: tasks,
+  },
+  acceptance: {
+    name: "Critère d'acceptation",
+    items: [],
+  },
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
+  toDo: {
+    name: "TODO",
+    items: [],
+  },
 
-    const items = Array.from(students);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+  inProgress: {
+    name: "In progress",
+    items: [],
+  },
+  done: {
+    name: "Done",
+    items: [],
+  },
+};
 
-    updateStudents(items);
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
   }
+};
 
-  const colors = [
-    "#f7d472",
-    "#cff772",
-    "#9ef772",
-    "#72f7d8",
-    "#72b7f7",
-    "#8872f7",
-    "#e372f7",
-    "#f7729c",
-  ];
-
+function Groups() {
+  const [columns, setColumns] = useState(taskStatus);
   return (
-    <div className="groups-content">
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="students">
-          {(provided) => (
-            <ul
-              className="students"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {students.map(({ id, username }, index) => {
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(provided) => (
-                      <li
-                        className="student"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={{
-                          backgroundColor: colors[index],
-                          color: "black",
-                          ...provided.draggableProps.style,
-                        }}
-                      >
-                        <p>{username}</p>
-                      </li>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-
-        <Droppable droppableId="groups">
-          {(provided) => (
-            <ul
-              className="groups"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Scrum Board</h1>
+      <div
+        style={{ display: "flex", justifyContent: "center", height: "100%" }}
+      >
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+                key={columnId}
+              >
+                {" "}
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          className="students"
+                          style={{
+                            padding: 4,
+                            width: 250,
+                            minHeight: 140,
+                            maxHeight: 500,
+                            overflow: "auto",
+                            height: "auto",
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      className="student"
+                                    >
+                                      Task
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+              </div>
+            );
+          })}
+        </DragDropContext>
+      </div>
     </div>
   );
 }
-
 export default Groups;
