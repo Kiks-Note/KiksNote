207f17bb8348dcacc0316332bd7d4214d4fc02e7
Ilan Petiot <ilan@blacklight.tv>
2023-03-20 15:39:37 +0100

    #33 fix / Possibility to change value without reloading page.

--- server/blog_back.js
+++ server/blog_back.js
@@ -1,56 +1,78 @@
-module.exports = (app, db) => {
-    // app.get("/blogs", (req, res) => {
-    //     const docs = [];
-    //     const comments = [];
-    //     res.setHeader("Access-Control-Allow-Origin", "*");
-    //     db.collection("blog_tutos").get().then((snapshot) => {
-    //         snapshot.forEach((doc) => {
-    //             docs.push(doc.data());
-    //             res.send(docs);
-    //             doc.ref.collection("comment").get().then((snap) => {
-    //                 snap.forEach((com) => {
-    //                     comments.push(com.data());
-    //                     // res.send(comments);
-    //                     // console.log(comments);
-    //                 })
-    //             }).catch((err) => {
-    //                 console.log(err);
-    //             });
-    //         })
-    //     })
-    // })
+module.exports = (app, db, ws) => {
+  // app.get("/blogs", (req, res) => {
+  //     const docs = [];
+  //     const comments = [];
+  //     res.setHeader("Access-Control-Allow-Origin", "*");
+  //     db.collection("blog_tutos").get().then((snapshot) => {
+  //         snapshot.forEach((doc) => {
+  //             docs.push(doc.data());
+  //             res.send(docs);
+  //             doc.ref.collection("comment").get().then((snap) => {
+  //                 snap.forEach((com) => {
+  //                     comments.push(com.data());
+  //                     // res.send(comments);
+  //                     // console.log(comments);
+  //                 })
+  //             }).catch((err) => {
+  //                 console.log(err);
+  //             });
+  //         })
+  //     })
+  // })
 
+  ws.on("request", (request) => {
+    const connection = request.accept(null, request.origin);
 
-    // get all tutos
-    app.get("/tutos", async (req, res) => {
-        const snapshot = await db.collection('blog_tutos').get();
-        res.send(snapshot.docs.map(doc => doc.data()));
-        // return snapshot.docs.map(doc => doc.data());
-    });
+    connection
+      ? console.log("connection ok")
+      : console.log("connection failed");
 
-    //get all tutos id
-    app.get("/tutos/id", async (req, res) => {
-        const snapshot = await db.collection('blog_tutos').get();
-        res.send(snapshot.docs.map(doc => doc.id));
-        // return snapshot.docs.map(doc => doc.data());
-    });
+    db.collection("blog_tutos").onSnapshot(
+      (snapshot) => {
+        const documents = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        connection.sendUTF(JSON.stringify(documents));
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  });
 
+  // get all tutos
+  // app.get("/tutos", async (req, res) => {
+  //   const snapshot = await db.collection("blog_tutos").get();
+  //   res.send(snapshot.docs.map((doc) => doc.data()));
+  //   // return snapshot.docs.map(doc => doc.data());
+  // });
 
-    // get tutos by id
-    app.get("/tuto/:id", async (req, res) => {
-        const snapshot = await db.collection('blog_tutos').doc(req.params.id).get();
-        res.send(snapshot.data());
-    });
+  //get all tutos id
+  app.get("/tutos/id", async (req, res) => {
+    const snapshot = await db.collection("blog_tutos").get();
+    res.send(snapshot.docs.map((doc) => doc.id));
+    // return snapshot.docs.map(doc => doc.data());
+  });
 
-    // get all comments by tutos id
-    app.get("/tuto/:id/comments", async (req, res) => {
-        const snapshot = await db.collection('blog_tutos').doc(req.params.id).collection("comment").get();
-        res.send(snapshot.docs.map(doc => doc.data()));
-    });
+  // get tutos by id
+  app.get("/tuto/:id", async (req, res) => {
+    const snapshot = await db.collection("blog_tutos").doc(req.params.id).get();
+    res.send(snapshot.data());
+  });
 
-    // app.post("/blog/:id/comments", async (req, res) => {
-    //     const snapshot = await db.collection('blog_tutos').doc(req.params.id).collection("comment").add(req.body);
-    //     res.send(snapshot.data());
-    // });
+  // get all comments by tutos id
+  app.get("/tuto/:id/comments", async (req, res) => {
+    const snapshot = await db
+      .collection("blog_tutos")
+      .doc(req.params.id)
+      .collection("comment")
+      .get();
+    res.send(snapshot.docs.map((doc) => doc.data()));
+  });
 
-}
+  // app.post("/blog/:id/comments", async (req, res) => {
+  //     const snapshot = await db.collection('blog_tutos').doc(req.params.id).collection("comment").add(req.body);
+  //     res.send(snapshot.data());
+  // });
+};
