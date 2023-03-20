7e82aa98164b84c21c2c8610a92390814081b700
N0ku <lucielecouturier2@gmail.com>
2023-03-20 23:28:49 +0100

    #20 - Update groups with some fixed case

--- client/src/Pages/groups/Groups.js
+++ client/src/Pages/groups/Groups.js
@@ -2,97 +2,27 @@ import "./Groups.scss";
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
-const tasks = [
-  {
-    id: "1",
-    name: "Board EduScrum",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  },
-  {
-    id: "2",
-    name: "Création de sprint agile",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  },
-  {
-    id: "3",
-    name: "BurnDown chart",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  },
-  {
-    id: "4",
-    name: "Ajout du backlog",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  },
-  {
-    id: "5",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  },
-  {
-    id: "6",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "7",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "8",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "9",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "10",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "11",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "12",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
-  {
-    id: "13",
-    name: "Sprint retro",
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  },
+const students = [{ id: "1", name: "Student 1" }, { id: "2", name: "Student 2" }];
+
+let groups = [
+  { id: "1", name: "Groupe 1", students: [] },
+  { id: "2", name: "Groupe 2", students: [] },
+  { id: "3", name: "Groupe 3", students: [] },
+  { id: "4", name: "Groupe 4", students: [] },
 ];
 
 const taskStatus = {
-  requested: {
-    name: "Stories",
-    items: tasks,
+  Students: {
+    name: "Eleves",
+    items: students,
   },
-  acceptance: {
-    name: "Critère d'acceptation",
-    items: [],
-  },
-
-  toDo: {
-    name: "TODO",
-    items: [],
-  },
-
-  inProgress: {
-    name: "In progress",
-    items: [],
-  },
-  done: {
-    name: "Done",
-    items: [],
-  },
+  ...groups.reduce((acc, group) => {
+    acc[group.id] = {
+      name: group.name,
+      items: group.students,
+    };
+    return acc;
+  }, {}),
 };
 
 const onDragEnd = (result, columns, setColumns) => {
@@ -105,6 +35,16 @@ const onDragEnd = (result, columns, setColumns) => {
     const destItems = [...destColumn.items];
     const [removed] = sourceItems.splice(source.index, 1);
     destItems.splice(destination.index, 0, removed);
+
+    // If the destination is a group, move the student into the group's student array
+    if (destination.droppableId !== "Students") {
+      groups = groups.map((group) => {
+        if (group.id === destination.droppableId) {
+          group.students = destItems;
+        }
+        return group;
+      });
+    }
 
     setColumns({
       ...columns,
@@ -151,11 +91,16 @@ function Groups() {
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
+                  margin: 8,
+                  border: "1px solid #ccc",
+                  borderRadius: 2,
+                  backgroundColor: "#f5f5f5",
+                  width: 250,
+                  minHeight: 500,
                 }}
                 key={columnId}
               >
-                {" "}
-                <h2>{column.name}</h2>
+                <h2 style={{ padding: "10px 0" }}>{column.name}</h2>
                 <div style={{ margin: 8 }}>
                   <Droppable droppableId={columnId} key={columnId}>
                     {(provided, snapshot) => {
@@ -163,14 +108,13 @@ function Groups() {
                         <div
                           {...provided.droppableProps}
                           ref={provided.innerRef}
-                          className="students"
                           style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "inherit",
                             padding: 4,
-                            width: 250,
-                            minHeight: 140,
-                            maxHeight: 500,
-                            overflow: "auto",
-                            height: "auto",
+                            width: "100%",
+                            minHeight: 400,
                           }}
                         >
                           {column.items.map((item, index) => {
@@ -186,9 +130,19 @@ function Groups() {
                                       ref={provided.innerRef}
                                       {...provided.draggableProps}
                                       {...provided.dragHandleProps}
-                                      className="student"
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#263B4A"
+                                          : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style,
+                                      }}
                                     >
-                                      Task
+                                      {item.name}
                                     </div>
                                   );
                                 }}
@@ -209,4 +163,5 @@ function Groups() {
     </div>
   );
 }
+
 export default Groups;
