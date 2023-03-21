eefe5ae454ec49e5f01b47ded5de0c71034b46a0
Ilan Petiot <ilan@blacklight.tv>
2023-03-21 09:22:09 +0100

    #33 fix / Rename functions.

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,39 +1,36 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test from "./pages/Test";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
 
 import Home from "./pages/home/Home.js";
-import Blog from "./pages/blog/Blog";
+import Tuto from "./pages/blog/Tuto";
 
 function RoutesProvider() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* EXAMPLES */}
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* EXAMPLES */}
 
-                {/* <Route path="/login" element={<Login />} /> */}
-                {/* <Route path="/register" element={<Register />} /> */}
-                {/* To Use the Navbar change Test by your page
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* To Use the Navbar change Test by your page
                 <Route path="/test" element={<Navbar element={<Test />} />} /> */}
-                
-                {/*<Route path="/" element={<Navbar element={<Test/>}/>}/>*/}
-                {/*<Route path="/test" element={<Navbar element={<Test/>}/>}/>*/}
 
+        {/*<Route path="/" element={<Navbar element={<Test/>}/>}/>*/}
+        {/*<Route path="/test" element={<Navbar element={<Test/>}/>}/>*/}
 
-                {/* Write here route that you don't need to be login*/}
+        {/* Write here route that you don't need to be login*/}
 
+        <Route element={<PrivateRoutes />}>
+          {/* Write here route that you need to be login*/}
 
-                <Route element={<PrivateRoutes/>}>
-                    {/* Write here route that you need to be login*/}
-
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/blog" element={<Blog/>}/>
-
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+          <Route path="/" element={<Home />} />
+          <Route path="/tuto" element={<Tuto />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default RoutesProvider;
