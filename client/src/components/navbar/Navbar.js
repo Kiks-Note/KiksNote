bb84646d495cc8d0103b2c4c6b24c9bf9213c1e0
Elim Florvil <elim.florvil@icloud.com>
2023-03-24 17:37:56 +0100

    #69 fix/ navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
@@ -30,28 +30,27 @@ import Container from "@mui/material/Container";
 import { accountAuthService } from "../../services/accountAuth";
 import { useNavigate } from "react-router-dom";
 
-
 const drawerWidth = 240;
 /// Drawer open style
 const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: "hidden",
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
 });
 /// Drawer close style
 const closedMixin = (theme) => ({
-    transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: "hidden",
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up("sm")]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
 });
 
 /// Drawer Header style
@@ -62,24 +61,23 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-
 }));
 /// Drawer style
 const Drawer = styled(MuiDrawer, {
-    shouldForwardProp: (prop) => prop !== "open",
-})(({theme, open}) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap",
-    boxSizing: "border-box",
-    ...(open && {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-    }),
-    ...(!open && {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-    }),
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
 }));
 
 export default function MiniDrawer({ element }) {
@@ -90,17 +88,17 @@ export default function MiniDrawer({ element }) {
   const navigate = useNavigate();
 
   const handleDrawerOpen = () => {
-      setOpen(true);
+    setOpen(true);
   };
   const handleDrawerClose = () => {
-      setOpen(false);
+    setOpen(false);
   };
 
   const handleLogout = () => {
     accountAuthService.logout();
-    localStorage.removeItem("userUid")
-    localStorage.removeItem("user")
-    navigate('/login');
+    localStorage.removeItem("userUid");
+    localStorage.removeItem("user");
+    navigate("/login");
   };
 
   return (
@@ -110,11 +108,17 @@ export default function MiniDrawer({ element }) {
         <DrawerHeader>
           {open ? (
             <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon style={{ color: theme.palette.custom.iconDrawer }} fontSize="large" />
+              <ChevronLeftIcon
+                style={{ color: theme.palette.custom.iconDrawer }}
+                fontSize="large"
+              />
             </IconButton>
           ) : (
             <IconButton onClick={handleDrawerOpen}>
-              <ChevronRightIcon style={{ color: theme.palette.custom.iconDrawer }} fontSize="large" />
+              <ChevronRightIcon
+                style={{ color: theme.palette.custom.iconDrawer }}
+                fontSize="large"
+              />
             </IconButton>
           )}
         </DrawerHeader>
@@ -125,31 +129,49 @@ export default function MiniDrawer({ element }) {
               id: 1,
               name: "Accueil",
               route: "/",
-              icon: <HomeIcon style={{ color: theme.palette.custom.iconDrawer }} />,
+              icon: (
+                <HomeIcon style={{ color: theme.palette.custom.iconDrawer }} />
+              ),
             },
             {
               id: 2,
               name: "Agile",
               route: "#",
-              icon: <BallotIcon style={{ color: theme.palette.custom.iconDrawer }} />,
+              icon: (
+                <BallotIcon
+                  style={{ color: theme.palette.custom.iconDrawer }}
+                />
+              ),
             },
             {
               id: 3,
-              name: "Profile",
+              name: "Profil",
               route: "#",
-              icon: <Person2Icon style={{ color: theme.palette.custom.iconDrawer }} />,
+              icon: (
+                <Person2Icon
+                  style={{ color: theme.palette.custom.iconDrawer }}
+                />
+              ),
             },
             {
               id: 4,
               name: "Espace de travail",
               route: "/tabList",
-              icon: <DashboardIcon style={{ color: theme.palette.custom.iconDrawer }} />,
+              icon: (
+                <DashboardIcon
+                  style={{ color: theme.palette.custom.iconDrawer }}
+                />
+              ),
             },
             {
               id: 5,
               name: "Blog",
-              route: "#",
-              icon: <AlternateEmailIcon style={{ color: theme.palette.custom.iconDrawer }} />,
+              route: "/blog",
+              icon: (
+                <AlternateEmailIcon
+                  style={{ color: theme.palette.custom.iconDrawer }}
+                />
+              ),
             },
           ].map((page) => (
             <ListItem key={page.id} disablePadding sx={{ display: "block" }}>
@@ -171,7 +193,10 @@ export default function MiniDrawer({ element }) {
                   {page.icon}
                 </ListItemIcon>
 
-                <ListItemText primary={page.name} sx={{ opacity: open ? 1 : 0 }} />
+                <ListItemText
+                  primary={page.name}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
               </ListItemButton>
             </ListItem>
           ))}
@@ -184,7 +209,11 @@ export default function MiniDrawer({ element }) {
               id: 1,
               name: "Déconnexion",
               route: "#",
-              icon: <LogoutOutlinedIcon style={{ color: theme.palette.custom.iconDrawer }} />,
+              icon: (
+                <LogoutOutlinedIcon
+                  style={{ color: theme.palette.custom.iconDrawer }}
+                />
+              ),
             },
           ].map((page) => (
             <ListItem key={page.id} disablePadding sx={{ display: "block" }}>
@@ -205,16 +234,27 @@ export default function MiniDrawer({ element }) {
                 >
                   {page.icon}
                 </ListItemIcon>
-                <ListItemText primary={page.name} sx={{ opacity: open ? 1 : 0 }} />
+                <ListItemText
+                  primary={page.name}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
               </ListItemButton>
             </ListItem>
           ))}
           {theme.mode === "dark" ? (
-            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+            <IconButton
+              sx={{ ml: 1 }}
+              onClick={colorMode.toggleColorMode}
+              color="inherit"
+            >
               <Brightness7Icon />
             </IconButton>
           ) : (
-            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+            <IconButton
+              sx={{ ml: 1 }}
+              onClick={colorMode.toggleColorMode}
+              color="inherit"
+            >
               <Brightness4Icon />
             </IconButton>
           )}
@@ -228,5 +268,5 @@ export default function MiniDrawer({ element }) {
 }
 
 MiniDrawer.propTypes = {
-    element: PropTypes.func,
+  element: PropTypes.func,
 };
