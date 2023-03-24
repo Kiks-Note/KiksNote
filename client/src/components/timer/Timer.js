0a158fea3845862725fe6d81b9badeb20c71cacf
N0ku <lucielecouturier2@gmail.com>
2023-03-24 10:18:15 +0100

    #24 fix/Timer warning and delete empty files

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,33 +1,31 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Timer() {
-  let minutes = 15;
-  let seconds = 0;
-  let generated = false;
-
-  let countDownDate = new Date(new Date().getTime() + 15 * 60000).getTime();
+  const [minutes, setMinutes] = useState(15);
+  const [seconds, setSeconds] = useState(0);
+  const [generated, setGenerated] = useState(false);
 
   useEffect(() => {
+    let countDownDate = new Date(new Date().getTime() + minutes * 60000).getTime();
+    let intervalId;
     if (!generated) {
-      var x = setInterval(function () {
+      intervalId = setInterval(function () {
         var now = new Date().getTime();
-
         var distance = countDownDate - now;
-
-        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("timer").innerHTML =
-          minutes + "m " + seconds + "s ";
-
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("timer").innerHTML = "Compteur terminé !";
-          document.getElementsByClassName("DivQr")[0].style.display = "none";
-        }
+        setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+        setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
       }, 1000);
-      generated = true;
+      setGenerated(true);
     }
-  }, []);
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(intervalId);
+      document.getElementById("timer").innerHTML = "Compteur terminé !";
+      document.getElementsByClassName("DivQr")[0].style.display = "none";
+    }
+    return () => clearInterval(intervalId);
+  }, [generated, minutes, seconds]);
+
   return <span id="timer">{minutes + "m " + seconds + "s "}</span>;
 }
+
 export default Timer;
