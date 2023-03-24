5e940502d83698b8bdd42a66f5d1f1a1098f0365
Jerance <michael.jerance@gmail.com>
2023-03-24 10:16:04 +0100

    #41 fix/ Fix server express return catch erros

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,6 @@
 module.exports = (
   app,
   db,
-  jwt,
   auth,
   authClient,
   signInWithEmailAndPassword
@@ -16,67 +15,14 @@ module.exports = (
             const customToken = await auth.createCustomToken(decodedToken.uid);
             const docRef = await db.collection("users");
             const snapshot = await docRef.doc(userCredential.user.uid).get();
-            res.status(200).send({ token: customToken , refreshToken : userCredential.user.stsTokenManager.refreshToken, user : snapshot.data()});
+            return res.status(200).send({ token: customToken, refreshToken: userCredential.user.stsTokenManager.refreshToken, user: snapshot.data() });
           });
         }
-      );
+      ).catch((error) => {
+        return res.status(401).send(error);
+      });
     } catch (err) {
-      res.status(401).send(err);
+      return res.status(401).send(err);
     }
-  });
-
-  app.post("/auth/refreshToken", async (req, res) => {
-    const authHeader = req.headers["authorization"];
-    const refreshTokenPosted = authHeader && authHeader.split(" ")[1];
-
-    if (!refreshTokenPosted) {
-      return res.sendStatus(401).send("Refresh token is missing");
-    }
-
-    jwt.verify(
-      refreshTokenPosted,
-      process.env.REFRESH_TOKEN_SECRET,
-      (err, user) => {
-        console.log(user);
-        if (err) {
-          return res.status(401).send("Refresh token is not valid");
-        }
-        delete user.iat;
-        delete user.exp;
-        const refreshedToken = generateAccessToken(user);
-        return res.status(200).send({
-          accessToken: refreshedToken,
-        });
-      }
-    );
-  });
-
-  function authenticateToken(req, res, next) {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) {
-      return res.sendStatus(401);
-    }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(401);
-      }
-      req.user = user;
-      next();
-    });
-  }
-
-  app.get("/auth/me", authenticateToken, (req, res) => {
-    return res.status(200).send(req.user);
-  });
-
-  app.get("/auth/users", async (req, res) => {
-    const docRef = await db.collection("users");
-    const snapshot = await docRef.get();
-    const users = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-
-    return res.status(200).send(users);
   });
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const dotenv = require('dotenv').config();
 const app = express();
 const { db, auth } = require("./firebase");
@@ -16,6 +14,6 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5050;
 
-require("./routes/auth")(app, db, jwt, auth, authClient, signInWithEmailAndPassword);
+require("./routes/auth")(app, db, auth, authClient, signInWithEmailAndPassword);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
