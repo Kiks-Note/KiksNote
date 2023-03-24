943a75bbab26628c32ab9f047e56f858affbbe4e
N0ku <lucielecouturier2@gmail.com>
2023-03-24 10:37:58 +0100

    #24 fix/conflics

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,11 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Appel from "./pages/call/Call";
 import Home from "./pages/home/Home";
 import Blog from "./pages/blog/Blog";
-import Login from "./pages/login/Login";
 import Presence from "./pages/presence/Presence";
 import Groups from "./pages/groups/Groups";
-import Register from "./pages/register/Register";
-
 import PrivateRoutes from "./utils/PrivateRoutes";
 import Navbar from "./components/navbar/Navbar";
 
@@ -14,17 +11,7 @@ function RoutesProvider() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         {/* EXAMPLES */}
-        {/* <Route path="/register" element={<Register />} /> */}
-        {/* To Use the Navbar change Test by your page
-                <Route path="/test" element={<Navbar element={<Test />} />} /> */}
-
-        {/*<Route path="/" element={<Navbar element={<Test/>}/>}/>*/}
-        {/*<Route path="/test" element={<Navbar element={<Test/>}/>}/>*/}
-
-        {/* <Route path="/register" element={<Register />} /> */}
 
         <Route element={<PrivateRoutes />}>
           <Route path="/appel" element={<Navbar element={<Appel />} />} />
@@ -33,7 +20,6 @@ function RoutesProvider() {
             element={<Navbar element={<Presence />} />}
           />
           <Route path="/groups" element={<Navbar element={<Groups />} />} />
-
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
         </Route>
