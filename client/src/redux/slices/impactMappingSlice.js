b5e1f7057adea52b2d46497ab2435120b0121364
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-03-28 02:16:16 +0200

    #51 refractor/agile adding delete function to redux slices of impact mapping

--- client/src/redux/slices/impactMappingSlice.js
+++ client/src/redux/slices/impactMappingSlice.js
@@ -4,14 +4,14 @@ import { createSlice } from '@reduxjs/toolkit';
 const impactMappingSlice = createSlice({
     name: 'impactMapping',
     initialState:{
-        goal:[],
+        goals:[],
         actors:[],
         impacts:[],
         deliverables:[]
 },
     reducers: {
         addImpactMappingGoals: (state, action) => {
-            state.goal.push(action.payload.goal)
+            state.goals.push(action.payload.goals)
         },
         addImpactMappingActors: (state, action) => {
             state.actors.push(action.payload.actors)
@@ -21,12 +21,25 @@ const impactMappingSlice = createSlice({
         },
         addImpactMappingDeliverables: (state, action) => {
             state.deliverables.push(action.payload.deliverables)
+        },
+        deleteImpactMappingGoals: (state, action) =>{
+            state.goals.splice(action.payload.index, 1)
+        },
+        deleteImpactMappingActors: (state, action) =>{
+            state.actors.splice(action.payload.index, 1)
+        },
+        deleteImpactMappingImpacts: (state, action) =>{
+            state.impacts.splice(action.payload.index, 1)
+        },
+        deleteImpactMappingDeliverables: (state, action) =>{
+            state.deliverables.splice(action.payload.index, 1)
         }
+
     }
 })
 
 //there you export the function to use it everywhere you want
-export const {addImpactMappingGoals, addImpactMappingActors, addImpactMappingDeliverables, addImpactMappingImpacts} = impactMappingSlice.actions
+export const {addImpactMappingGoals, addImpactMappingActors, addImpactMappingDeliverables, addImpactMappingImpacts, deleteImpactMappingActors, deleteImpactMappingDeliverables, deleteImpactMappingGoals, deleteImpactMappingImpacts} = impactMappingSlice.actions
 
 
 export default impactMappingSlice.reducer
