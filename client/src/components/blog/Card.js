cb94158f1cbcf6f9614369ec95e0ed79457e3dc8
Elim Florvil <elim.florvil@icloud.com>
2023-04-17 16:57:25 +0200

    #69 fix /on card

--- client/src/components/blog/Card.js
+++ client/src/components/blog/Card.js
@@ -76,15 +76,6 @@ export default function ImgMediaCard({
       {type === "tuto" ? (
         <Grid item xs={2} sm={4} md={5}>
           <Card sx={{ maxWidth: 380, m: 2 }}>
-            <CardMedia
-
-              component="img"
-              // alt="green iguana"
-              height="10"
-              image={image}
-            // image="https://cdn.code.daypilot.org/image/big/7sca734yufgatkpbudmqro7tga/vue-resource-calendar-open-source.png"
-            // sx={{maxHeight: 250}}
-            />
             <CardMedia component="img" height="10" image={image} />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
@@ -98,8 +89,6 @@ export default function ImgMediaCard({
                 {description}
               </Typography>
             </CardContent>
-
-
             <CardActions>
               <Stack direction="column">
                 <Stack direction="row" paddingLeft="10px">
@@ -146,62 +135,8 @@ export default function ImgMediaCard({
           </Card>
         </Grid>
       ) : (
-        <Grid item xs={2} sm={4} md={5}>
-          <Card sx={{ maxWidth: 380, m: 2 }}>
-            <CardMedia
-              component="img"
-              // alt="green iguana"
-              height="10"
-              image={image}
-            // image="https://cdn.code.daypilot.org/image/big/7sca734yufgatkpbudmqro7tga/vue-resource-calendar-open-source.png"
-            // sx={{maxHeight: 250}}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {title}
-                {/*yo*/}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {description}
-                {/*sisi*/}
-              </Typography>
-
-            </CardContent>
-            <CardContent>
-              <Typography color="black">
-                {event}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              {like}
-              <Checkbox
-                icon={<ThumbUpOffAltIcon />}
-                checkedIcon={<ThumbUpIcon />}
-              />
-              {dislike}
-              <Checkbox
-                icon={<ThumbDownOffAltIcon />}
-                checkedIcon={<ThumbDownAltIcon color={"error"} />}
-              />
-
-              <Button size="small">Commencer</Button>
-              <Button size="small"
-
-                onClick={handleClick}
-
-              >Ouvrir</Button>
-              <Button onClick={
-                deleteBlog
-              }
-
-              >Supprimer</Button>
-
-            </CardActions>
-          </Card>
-        </Grid>
+        <h1>Blog</h1>
       )}
     </>
   );
 }
-
-
