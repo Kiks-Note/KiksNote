8c6e8d6d5de0382d2014c635b223318c5d7be6f7
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-05-09 00:48:50 +0200

    #51 feat/impact-mapping adding the color option to redux

--- client/src/redux/slices/impactMappingSlice.js
+++ client/src/redux/slices/impactMappingSlice.js
@@ -1,45 +1,73 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
+const DEFAULT_COLOR = ""
 //This is the redux store, this will help to store our impact mapping card
 const impactMappingSlice = createSlice({
-    name: 'impactMapping',
-    initialState:{
-        goals:[],
-        actors:[],
-        impacts:[],
-        deliverables:[]
-},
-    reducers: {
-        addImpactMappingGoals: (state, action) => {
-            state.goals.push(action.payload.goals)
-        },
-        addImpactMappingActors: (state, action) => {
-            state.actors.push(action.payload.actors)
-        },
-        addImpactMappingImpacts: (state, action) => {
-            state.impacts.push(action.payload.impacts)
-        },
-        addImpactMappingDeliverables: (state, action) => {
-            state.deliverables.push(action.payload.deliverables)
-        },
-        deleteImpactMappingGoals: (state, action) =>{
-            state.goals.splice(action.payload.index, 1)
-        },
-        deleteImpactMappingActors: (state, action) =>{
-            state.actors.splice(action.payload.index, 1)
-        },
-        deleteImpactMappingImpacts: (state, action) =>{
-            state.impacts.splice(action.payload.index, 1)
-        },
-        deleteImpactMappingDeliverables: (state, action) =>{
-            state.deliverables.splice(action.payload.index, 1)
-        }
-
-    }
-})
+  name: "impactMapping",
+  initialState: {
+    goals: [],
+    actors: [],
+    impacts: [],
+    deliverables: [],
+  },
+  reducers: {
+    addImpactMappingGoals: (state, action) => {
+      const { text, color } = action.payload;
+      const newGoal = {
+        text: text,
+        color: color || DEFAULT_COLOR, 
+      };
+      state.goals.push(newGoal);
+    },
+    addImpactMappingActors: (state, action) => {
+      const { text, color } = action.payload;
+      const newActor = {
+        text: text,
+        color: color || DEFAULT_COLOR,
+      };
+      state.actors.push(newActor);
+    },
+    addImpactMappingImpacts: (state, action) => {
+      const { text, color } = action.payload;
+      const newImpact = {
+        text: text,
+        color: color || DEFAULT_COLOR,
+      };
+      state.impacts.push(newImpact);
+    },
+    addImpactMappingDeliverables: (state, action) => {
+      const { text, color } = action.payload;
+      const newDeliverable = {
+        text: text,
+        color: color || DEFAULT_COLOR,
+      };
+      state.deliverables.push(newDeliverable);
+    },
+    deleteImpactMappingGoals: (state, action) => {
+      state.goals.splice(action.payload.index, 1);
+    },
+    deleteImpactMappingActors: (state, action) => {
+      state.actors.splice(action.payload.index, 1);
+    },
+    deleteImpactMappingImpacts: (state, action) => {
+      state.impacts.splice(action.payload.index, 1);
+    },
+    deleteImpactMappingDeliverables: (state, action) => {
+      state.deliverables.splice(action.payload.index, 1);
+    },
+  },
+});
 
 //there you export the function to use it everywhere you want
-export const {addImpactMappingGoals, addImpactMappingActors, addImpactMappingDeliverables, addImpactMappingImpacts, deleteImpactMappingActors, deleteImpactMappingDeliverables, deleteImpactMappingGoals, deleteImpactMappingImpacts} = impactMappingSlice.actions
+export const {
+  addImpactMappingGoals,
+  addImpactMappingActors,
+  addImpactMappingDeliverables,
+  addImpactMappingImpacts,
+  deleteImpactMappingActors,
+  deleteImpactMappingDeliverables,
+  deleteImpactMappingGoals,
+  deleteImpactMappingImpacts,
+} = impactMappingSlice.actions;
 
-
-export default impactMappingSlice.reducer
+export default impactMappingSlice.reducer;
