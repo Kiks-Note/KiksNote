67d88416547754aaf5ecc200d68928827373692e
Elim Florvil <elim.florvil@icloud.com>
2023-05-09 11:32:35 +0200

    #78 style on functional tree

--- client/src/pages/agile/ArbreFonctionnel.js
+++ client/src/pages/agile/ArbreFonctionnel.js
@@ -1,159 +1,155 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { makeStyles } from "@mui/styles";
-import Tree from 'react-d3-tree';
+import Tree from "react-d3-tree";
 
-
-import { Drawer, List, ListItem, ListItemText, Typography, IconButton} from "@mui/material";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  IconButton,
+} from "@mui/material";
 
 import { ExpandMore, ChevronRight, Delete } from "@mui/icons-material";
 
-import { TreeView,TreeItem } from '@mui/lab';
+import { TreeView, TreeItem } from "@mui/lab";
 
 import Arbre from "../../components/agile/Arbre.js";
 
-
-
-
-const drawerWidth = 240;
+const drawerWidth = 160;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-        
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        height: '100%',
-    },
-    content: {
-        flexGrow: 1,
-
-    },
-    main:{backgroundcolor:'white!',width: '100%',
-    height: '100%'},
-    main:{},
-    ro: {
-        flexGrow: 1,
-    }
+  root: {
+    display: "flex",
+    width: "100 vh",
+    height: "100%",
+    background:"white",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    left: "auto",
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    height: "100%",
+  },
+  content: {
+    flexGrow: 1,
+  },
 }));
 function ArbreFonctionnel() {
-    const classes = useStyles();
-    const [selectedProject, setSelectedProject] = useState(null);
+  const classes = useStyles();
+  const [selectedProject, setSelectedProject] = useState(null);
 
-    const handleProjectSelect = (project) => {
-        console.log(project)
-        setSelectedProject(project);
-    };
+  const handleProjectSelect = (project) => {
+    console.log(project);
+    setSelectedProject(project);
+  };
 
-    const projects = [
-        { id: 1, name: 'Nouveau projet', content:{
-            name: 'Root',
-            children: [
-            ],
-          } },
-        { id: 2, name: 'Projet 2', content: {
-            name: 'Root',
-            children: [
-                {
-                    name: 'Node 1',
-                    children: [
-                        
-                    ],
-                },
-                {
-                    name: 'Node 2',
-                },
-            ],
-        } },
-        { id: 3, name: 'Projet 3', content: {
-            name: 'Root',
-            children: [
-                {
-                    name: 'Node 1',
-                    children: [
-                        
-                    ],
-                },
-                
-            ],
-        } },
-    ];
+  const projects = [
+    {
+      id: 1,
+      name: "Nouveau projet",
+      content: {
+        name: "Root",
+        children: [],
+      },
+    },
+    {
+      id: 2,
+      name: "Projet 2",
+      content: {
+        name: "Root",
+        children: [
+          {
+            name: "Node 1",
+            children: [],
+          },
+          {
+            name: "Node 2",
+          },
+        ],
+      },
+    },
+    {
+      id: 3,
+      name: "Projet 3",
+      content: {
+        name: "Root",
+        children: [
+          {
+            name: "Node 1",
+            children: [],
+          },
+        ],
+      },
+    },
+  ];
 
+  const handleDelete = (event, nodeId) => {
+    // Logic to delete the node
+    console.log("Deleting node:", nodeId);
+  };
 
-
-   
-
-    const handleDelete = (event, nodeId) => {
-        // Logic to delete the node
-        console.log('Deleting node:', nodeId);
-    };
-
-    const renderTree = (nodes) => (
-        <TreeItem
-            key={nodes.id}
-            nodeId={nodes.id}
-            label={
-                <div>
-                    <Typography variant="body1">{nodes.label}</Typography>
-                    <IconButton
-                        aria-label="delete"
-                        onClick={(event) => handleDelete(event, nodes.id)}
-                    >
-                        <Delete fontSize="small" />
-                    </IconButton>
-                </div>
-            }
-        >
-            {Array.isArray(nodes.children)
-                ? nodes.children.map((node) => renderTree(node))
-                : null}
-        </TreeItem>
-    );
-
-   
-
-    return (
-        <div className={classes.root}>
-            {/* Barre de sélection des projets */}
-            <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-
-                <div className={classes.drawerContainer}>
-                    <List>
-                        {projects.map((project) => (
-                            <ListItem
-                                button
-                                key={project.id}
-                                onClick={() => handleProjectSelect(project)}
-                            >
-                                <ListItemText primary={project.name} />
-                            </ListItem>
-                        ))}
-                    </List>
-                </div>
-            </Drawer>
-
-            {/* Mode d'édition du projet sélectionné */}
-            <main className={classes.main}>
-                {selectedProject ? (
-                   <Arbre projet={selectedProject}></Arbre>
-                ) : (
-                    <Typography variant="h5">Sélectionnez un projet ou cré</Typography>
-                )}
-            </main>
+  const renderTree = (nodes) => (
+    <TreeItem
+      key={nodes.id}
+      nodeId={nodes.id}
+      label={
+        <div>
+          <Typography variant="body1">{nodes.label}</Typography>
+          <IconButton
+            aria-label="delete"
+            onClick={(event) => handleDelete(event, nodes.id)}
+          >
+            <Delete fontSize="small" />
+          </IconButton>
         </div>
-    );
+      }
+    >
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
+  return (
+    <div className={classes.root}>
+      {/* Barre de sélection des projets */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerContainer}>
+          <List>
+            {projects.map((project) => (
+              <ListItem
+                button
+                key={project.id}
+                onClick={() => handleProjectSelect(project)}
+              >
+                <ListItemText primary={project.name} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+
+      {/* Mode d'édition du projet sélectionné */}
+      <div className={classes.main}>
+        {selectedProject ? (
+          <Arbre projet={selectedProject}></Arbre>
+        ) : (
+          <Typography variant="h5">Sélectionnez un projet ou crée</Typography>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ArbreFonctionnel;
