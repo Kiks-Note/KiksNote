84752da9cd90d7282d4191b174b647e220fd7be5
Elim Florvil <elim.florvil@icloud.com>
2023-05-16 00:34:15 +0200

    style on PreviewBlog

--- client/src/components/blog/PreviewBlog.js
+++ client/src/components/blog/PreviewBlog.js
@@ -1,56 +1,64 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
-import { EditorState, ContentState } from 'draft-js';
-import htmlToDraft from 'html-to-draftjs';
-import { Button } from '@mui/material';
-
-
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Button,
+  Typography,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
+import { EditorState, ContentState } from "draft-js";
+import htmlToDraft from "html-to-draftjs";
 
 function PreviewBlog() {
-    const [data, setData] = useState(null);
-    const { id } = useParams();
+  const [data, setData] = useState(null);
+  const { id } = useParams();
 
+  useEffect(() => {
+    (async () => {
+      console.log(id);
 
-    useEffect(() => {
-        (async () => {
+      await axios
+        .get(`http://localhost:5050/blog/${id}`)
+        .then((res) => {
+          setData(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })();
+  }, []);
 
-            console.log(id);
-
-            await axios.get(`http://localhost:5050/blog/${id}`).then((res) => {
-                setData(res.data);
-                console.log(res.data);
-            }).catch((err) => {
-                console.log(err);
-            });
-        })();
-    }, []);
-
-
-
-    return (
-        <div>
-            {data && (
-
-                //<h1>{data.inputEditorState}</h1>
-                <div dangerouslySetInnerHTML={{ __html: data.inputEditorState }} />
-
-
-
-            )}
-            <br />
-            <Button variant="contained"
-
-                onClick={
-                    () => {
-                        window.history.back();
-                    }
-                }>Retour à la page de blog </Button>
-        </div>
-    );
-
+  return (
+    <div>
+      <Button
+        variant="contained"
+        onClick={() => {
+          window.history.back();
+        }}
+      >
+        Retour à la page de blog{" "}
+      </Button>
+      {data && (
+        <Card>
+          <CardHeader title={data.title} />
+          <CardMedia
+            component="img"
+            src={data.photo}
+            alt={data.title}
+            sx={{ width: 300, height: 200 }}
+          />
+          <CardContent>
+            <div dangerouslySetInnerHTML={{ __html: data.inputEditorState }} />
+          </CardContent>
+        </Card>
+      )}
+      <br />
+    </div>
+  );
 }
 
 export default PreviewBlog;
