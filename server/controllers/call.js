b84bf0c6786f9d6c5df08a1fc8841c7492e059f6
Mohamed <mohameddrame907@gmail.com>
2023-05-18 16:29:48 +0200

    feat/mail send functionnality is set

--- server/controllers/call.js
+++ server/controllers/call.js
@@ -1,10 +1,31 @@
 const { db } = require("../firebase");
+var nodemailer = require('nodemailer');
 
 const exportMailCall = async (req, res) => {
-  console.log("222");
-  res.status(200)
-  res.send('AAAA')
- //   console.log("send: ", req.body.callfile)
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'services.kiksnote.noreply@gmail.com',
+      pass: "njujpddhfbazaifo"
+    }
+  });
+
+  var mailOptions = {
+    from: 'services.kiksnote.noreply@gmail.com',
+    to: "mohamed.niaissa@edu.esiee-it.fr",
+    subject: 'Récupération du mot de passe',
+    text: `Test text`
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
 };
 
 const callRequests = async (connection) => {
