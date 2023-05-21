fd0dc0dedeea626dcbe066fc80300c0830d8cb5e
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-05-21 17:59:08 +0200

    fix/redux fixing the redux tools in my indexjs

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,11 @@ import React, {useEffect} from "react";
 import ReactDOM from "react-dom/client";
 import {AuthProvider} from "./hooks/useAuth";
 import { Provider } from 'react-redux'
+import { store } from './redux/store';
 import Theme from "./utils/Theme";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 
 root.render(
   <Provider store={store}>
