c39098e5b52c41db2c95dff167d2e6c29235fabd
Elim Florvil <elim.florvil@icloud.com>
2023-05-21 22:57:13 +0200

    #81 feat/form && card for persona

--- client/src/pages/agile/Personas.js
+++ client/src/pages/agile/Personas.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Button, TextField, Typography } from "@mui/material";
-import { useForm, Controller } from "react-hook-form";
+import {
+  Button,
+  TextField,
+  Typography,
+  Select,
+  MenuItem,
+  InputLabel,
+  TextareaAutosize,
+  Grid,
+  Box,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import CardPersona from "../../components/agile/CardPersona";
 
 const schema = yup.object().shape({
+  gender: yup
+    .string()
+    .oneOf(["Masculin", "Féminin"])
+    .required("Veuillez sélectionner un genre"),
   firstName: yup.string().required("Votre persona a besoin d'un prénom"),
   lastName: yup.string().required("Votre persona a besoin d'un nom"),
   email: yup
@@ -24,18 +39,19 @@ const schema = yup.object().shape({
     .required("Votre persona a besoin de spécifier des besoins"),
 });
 
-const PersonaBuilder = () => {
-  const [persona, setPersona] = useState(null);
+export default function PersonaBuilder() {
+  const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
 
   const {
-    control,
     handleSubmit,
+    register,
     formState: { errors },
+    reset,
+    clearErrors,
   } = useForm({
     resolver: yupResolver(schema),
   });
-
   useEffect(() => {
     fetchPersonas();
   }, []);
@@ -46,11 +62,12 @@ const PersonaBuilder = () => {
       const response = await fetch("https://randomuser.me/api/?results=1");
       const data = await response.json();
       const { name, picture, gender, email, dob, location } = data.results[0];
-      const persona = {
+      const defineSexe = gender === "male" ? "Masculin" : "Féminin";
+      const newFormData = {
         firstName: name.first,
         lastName: name.last,
         picture: picture.large,
-        gender,
+        gender: defineSexe,
         email,
         age: dob.age,
         city: location.city,
@@ -60,8 +77,8 @@ const PersonaBuilder = () => {
         frustrations: "",
         needs: "",
       };
-      console.log(persona);
-      setPersona(persona);
+      reset();
+      setFormData(newFormData);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching personas:", error);
@@ -70,17 +87,14 @@ const PersonaBuilder = () => {
   };
 
   const handleInputChange = (field, value) => {
-    setPersona((prevPersona) => ({
-      ...prevPersona,
-      //   name: {
-      //     ...prevPersona.name,
-      //     [field]: value,
-      //   },
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: value,
     }));
   };
 
   const savePersona = (data) => {
-    console.log("Persona sauvegardé :", data);
+    console.log("Persona à sauvegarder :", data);
   };
 
   return (
@@ -93,174 +107,248 @@ const PersonaBuilder = () => {
           <Button variant="contained" onClick={fetchPersonas}>
             Générer un persona
           </Button>
-          {persona && (
-            <form onSubmit={handleSubmit(savePersona)}>
-              <div>
-                <Typography variant="h2">
-                  {persona.firstName} {persona.lastName}
-                </Typography>
-                <img src={persona.picture} alt="Avatar" />
-                <Typography variant="body1">Sexe: {persona.gender}</Typography>
-                <Typography variant="body1">Email: {persona.email}</Typography>
-                <Typography variant="body1">Age: {persona.age}</Typography>
-                <Typography variant="body1">
-                  Ville: {`${persona.city}, ${persona.country}`}
-                </Typography>
-                <Typography variant="body1">Bio: {persona.bio}</Typography>
-                <Typography variant="body1">
-                  Frustrations: {persona.frustrations}
-                </Typography>
-                <Typography variant="body1">
-                  Objectifs: {persona.objectives}
-                </Typography>
-                <Typography variant="body1">
-                  Besoins: {persona.needs}
-                </Typography>
-              </div>
-              <div style={{ marginTop: 40 }}>
-                <Controller
-                  name="firstName"
-                  control={control}
-                  defaultValue={persona.firstName}
-                  render={({ field }) => (
+          {formData && (
+            <>
+              <CardPersona info={formData} />
+              <Box
+                sx={{
+                  marginTop: 2,
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+                component="form"
+                encType="multipart/form-data"
+                noValidate
+                autoComplete="off"
+                onSubmit={handleSubmit(savePersona)}
+              >
+                <Grid container spacing={1}>
+                  <Grid item xs={12}>
+                    <InputLabel id="gender-label">Sexe</InputLabel>
+                    <Select
+                      labelId="gender-label"
+                      id="gender-select"
+                      value={formData.gender}
+                      name="gender"
+                      label="Sexe"
+                      {...register("gender")} // Enregistrer le champ avec react-hook-form
+                      error={!!errors.gender}
+                      onChange={(e) =>
+                        handleInputChange("gender", e.target.value)
+                      }
+                    >
+                      <MenuItem value="Masculin">Masculin</MenuItem>
+                      <MenuItem value="Féminin">Féminin</MenuItem>
+                    </Select>
+                    {errors.gender && (
+                      <Typography variant="body2" color="error">
+                        {errors.gender.message}
+                      </Typography>
+                    )}
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Prénom"
+                      name="firstName"
+                      {...register("firstName")}
+                      defaultValue={formData.firstName}
                       error={!!errors.firstName}
                       helperText={errors.firstName?.message}
                       onChange={(e) =>
                         handleInputChange("firstName", e.target.value)
                       }
                     />
-                  )}
-                />
-                <Controller
-                  name="lastName"
-                  control={control}
-                  defaultValue={persona.lastName}
-                  render={({ field }) => (
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Nom"
+                      name="lastName"
+                      {...register("lastName")}
+                      defaultValue={formData.lastName}
                       error={!!errors.lastName}
                       helperText={errors.lastName?.message}
                       onChange={(e) =>
                         handleInputChange("lastName", e.target.value)
                       }
                     />
-                  )}
-                />
-                <Controller
-                  name="email"
-                  control={control}
-                  defaultValue={persona.email}
-                  render={({ field }) => (
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Email"
+                      name="email"
+                      {...register("email")}
+                      defaultValue={formData.email}
                       error={!!errors.email}
                       helperText={errors.email?.message}
+                      onChange={(e) =>
+                        handleInputChange("email", e.target.value)
+                      }
                     />
-                  )}
-                />
-                <Controller
-                  name="age"
-                  control={control}
-                  defaultValue={persona.age}
-                  render={({ field }) => (
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Âge"
+                      name="age"
+                      {...register("age")}
                       type="number"
+                      defaultValue={formData.age}
                       error={!!errors.age}
                       helperText={errors.age?.message}
+                      onChange={(e) => handleInputChange("age", e.target.value)}
                     />
-                  )}
-                />
-                <Controller
-                  name="city"
-                  control={control}
-                  defaultValue={persona.city}
-                  render={({ field }) => (
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Ville"
+                      name="city"
+                      {...register("city")}
+                      defaultValue={formData.city}
                       error={!!errors.city}
                       helperText={errors.city?.message}
+                      onChange={(e) =>
+                        handleInputChange("city", e.target.value)
+                      }
                     />
-                  )}
-                />
-                <Controller
-                  name="country"
-                  control={control}
-                  defaultValue={persona.country}
-                  render={({ field }) => (
+                  </Grid>
+                  <Grid item xs={4}>
                     <TextField
-                      {...field}
                       label="Pays"
+                      name="country"
+                      {...register("country")}
+                      defaultValue={formData.country}
                       error={!!errors.country}
                       helperText={errors.country?.message}
+                      onChange={(e) =>
+                        handleInputChange("country", e.target.value)
+                      }
                     />
-                  )}
-                />
-                <Controller
-                  name="bio"
-                  control={control}
-                  defaultValue={persona.bio}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      label="Bio"
-                      error={!!errors.bio}
-                      helperText={errors.bio?.message}
-                      onChange={(e) => handleInputChange("bio", e.target.value)}
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <InputLabel id="bio">Bio *</InputLabel>
+                    <TextareaAutosize
+                      name="bio"
+                      {...register("bio")}
+                      minRows={2}
+                      maxRows={4}
+                      defaultValue={formData.bio}
+                      style={{
+                        width: "100%",
+                        background: "transparent",
+                        border: errors.bio ? "1px solid red" : "1px solid",
+                        borderRadius: "10px",
+                      }}
+                      onChange={(e) => {
+                        handleInputChange("bio", e.target.value);
+                        clearErrors("bio"); // Efface les erreurs lors de la modification du champ
+                      }}
                     />
-                  )}
-                />
-                <Controller
-                  name="objectives"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      label="Objectifs"
-                      error={!!errors.objectives}
-                      helperText={errors.objectives?.message}
+                    {errors.bio && (
+                      <span style={{ color: "red" }}>{errors.bio.message}</span>
+                    )}
+                  </Grid>
+                  <Grid item xs={6}>
+                    <InputLabel id="objectives">Objectifs *</InputLabel>
+                    <TextareaAutosize
+                      name="objectives"
+                      {...register("objectives")}
+                      minRows={2}
+                      maxRows={4}
+                      defaultValue={formData.objectives}
+                      style={{
+                        width: "100%",
+                        background: "transparent",
+                        border: errors.objectives
+                          ? "1px solid red"
+                          : "1px solid",
+                        borderRadius: "10px",
+                      }}
+                      onChange={(e) => {
+                        handleInputChange("objectives", e.target.value);
+                        clearErrors("objectives"); // Efface les erreurs lors de la modification du champ
+                      }}
                     />
-                  )}
-                />
-                <Controller
-                  name="frustrations"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      label="Frustrations"
-                      error={!!errors.frustrations}
-                      helperText={errors.frustrations?.message}
+                    {errors.objectives && (
+                      <span style={{ color: "red" }}>
+                        {errors.objectives.message}
+                      </span>
+                    )}
+                  </Grid>
+                  <Grid item xs={6}>
+                    <InputLabel id="frustrations">Frustrations *</InputLabel>
+                    <TextareaAutosize
+                      name="frustrations"
+                      {...register("frustrations")}
+                      minRows={2}
+                      maxRows={4}
+                      defaultValue={formData.frustrations}
+                      style={{
+                        width: "100%",
+                        background: "transparent",
+                        border: errors.frustrations
+                          ? "1px solid red"
+                          : "1px solid",
+                        borderRadius: "10px",
+                      }}
+                      onChange={(e) => {
+                        handleInputChange("frustrations", e.target.value);
+                        clearErrors("frustrations");
+                      }}
                     />
-                  )}
-                />
-                <Controller
-                  name="needs"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      label="Besoins"
-                      error={!!errors.needs}
-                      helperText={errors.needs?.message}
+                    {errors.frustrations && (
+                      <span style={{ color: "red" }}>
+                        {errors.frustrations.message}
+                      </span>
+                    )}
+                  </Grid>
+                  <Grid item xs={6}>
+                    <InputLabel id="besoins">Besoins *</InputLabel>
+                    <TextareaAutosize
+                      name="needs"
+                      {...register("needs")}
+                      minRows={2}
+                      maxRows={4}
+                      defaultValue={formData.needs}
+                      style={{
+                        width: "100%",
+                        background: "transparent",
+                        border: errors.needs ? "1px solid red" : "1px solid",
+                        borderRadius: "10px",
+                      }}
+                      onChange={(e) => {
+                        handleInputChange("needs", e.target.value);
+                        clearErrors("needs");
+                      }}
                     />
-                  )}
-                />
-              </div>
-              <Button type="submit" variant="contained">
-                Sauvegarder
-              </Button>
-            </form>
+                    {errors.needs && (
+                      <span style={{ color: "red" }}>
+                        {errors.needs.message}
+                      </span>
+                    )}
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "row ",
+                        justifyContent: "space-evenly",
+                      }}
+                    >
+                      <Button
+                        variant="contained"
+                        color="success"
+                        type="submit"
+                        sx={{ mt: 3, mb: 2 }}
+                      >
+                        Sauvegarder
+                      </Button>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            </>
           )}
         </>
       )}
@@ -268,4 +356,3 @@ const PersonaBuilder = () => {
   );
 };
 
-export default PersonaBuilder;
