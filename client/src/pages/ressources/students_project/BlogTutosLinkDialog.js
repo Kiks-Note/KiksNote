661b15bbe5420fd3ac2b53cd08f9a069928db9a5
Jerance <michael.jerance@gmail.com>
2023-05-31 01:51:56 +0200

    #89 fix/Optimize grid view with radios buttons for blog tutos to link project student

--- client/src/pages/ressources/students_project/BlogTutosLinkDialog.js
+++ client/src/pages/ressources/students_project/BlogTutosLinkDialog.js
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-
 import axios from "axios";
-
 import {
-  Autocomplete,
   Button,
-  TextField,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
   Grid,
   Typography,
+  Radio,
+  FormControlLabel,
 } from "@mui/material";
 
 const BlogTutosLinkDialog = (props) => {
   const { projectid } = useParams();
-
   const [selectedBlogTutoId, setSelectedBlogTutoId] = useState("");
 
   const publishStudentProject = async () => {
@@ -35,47 +32,65 @@ const BlogTutosLinkDialog = (props) => {
     } catch (error) {
       throw error;
     }
+    props.getStudentProjectById();
   };
 
-  const handleBlogTutoSelect = (event, value) => {
-    setSelectedBlogTutoId(value?.id || "");
+  const handleBlogTutoSelect = (event) => {
+    setSelectedBlogTutoId(event.target.value || "");
+  };
+
+  const handleImageClick = (blogTutoId) => {
+    setSelectedBlogTutoId(blogTutoId);
   };
 
   return (
-    <Dialog open={props.open} onClose={props.handleClose}>
+    <Dialog open={props.open} onClose={props.close}>
       <DialogTitle>Lier à un article blog tuto</DialogTitle>
       <DialogContent>
-        <Grid container spacing={2}>
-          {props.allblogtutos.map((blogTuto) => (
-            <Grid item key={blogTuto.id} xs={12} sm={6} md={4}>
-              <img
-                src={blogTuto.data.thumbnail}
-                style={{ width: "100%" }}
-                alt="oez"
-              />
-              <Typography variant="subtitle1" align="center">
-                {blogTuto.data.title}
-              </Typography>
-            </Grid>
-          ))}
-        </Grid>
-        <Autocomplete
-          options={props.allblogtutos}
-          getOptionLabel={(blogTuto) => blogTuto.data.title}
-          onChange={handleBlogTutoSelect}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Sélectionner un blog tuto"
-              variant="outlined"
-            />
-          )}
-        />
+        <div style={{ height: "600px", overflow: "auto" }}>
+          <Grid container spacing={2}>
+            {props.allblogtutos.map((blogTuto) => (
+              <Grid item key={blogTuto.id} xs={12} sm={6} md={4}>
+                <div style={{ position: "relative" }}>
+                  <FormControlLabel
+                    value={blogTuto.id}
+                    control={
+                      <Radio
+                        color="primary"
+                        checked={selectedBlogTutoId === blogTuto.id}
+                        onChange={handleBlogTutoSelect}
+                      />
+                    }
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      right: 0,
+                      zIndex: 1,
+                    }}
+                  />
+                  <img
+                    src={blogTuto.data.thumbnail}
+                    style={{ width: "100%", cursor: "pointer" }}
+                    alt="blog-tuto-linked-img"
+                    onClick={() => handleImageClick(blogTuto.id)}
+                  />
+                </div>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ textAlign: "center", marginBottom: "15px" }}
+                >
+                  {blogTuto.data.title}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </DialogContent>
       <DialogActions>
         <Button
           onClick={publishStudentProject}
           sx={{ backgroundColor: "#7a52e1", color: "white" }}
+          disabled={!selectedBlogTutoId}
         >
           Soumettre
         </Button>
