ba2cfe598a51b4772ab0d1714ab6046cefef0fef
Jerance <michael.jerance@gmail.com>
2023-06-01 01:39:00 +0200

    #94 wip/Jpo front in progress

--- client/src/pages/ressources/jpo/jpo.js
+++ client/src/pages/ressources/jpo/jpo.js
@@ -1,100 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import useFirebase from "../../../hooks/useFirebase";
 
-export default function Jpo(){
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse,
+  Divider,
+} from "@mui/material";
 
-    const [events, setEvent] = useState([]);
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
-    useEffect(() => {
-        const fetchEvent = async () => {
-            const response = await axios.get("http://localhost:5050/jpo")
-                .then((response) => {
-                    setEvent(response.data);
-                });
-        };
-        fetchEvent();
-    }, []);
+import "./jpo.scss";
 
-    return (
-        <div>
-            <h1>JPO</h1>
-            <Button variant="contained" disableElevation sx={{
-                marginLeft: "80%",
-            }}>
-                Soumettre un projet
-            </Button>
-            <div className="grid grid-rows-3 grid-flow-col gap-4" style={{ width: "100%", height: "100%" }}>
-                <div className="row-span-3 ..." 
-                    style={{
-                        backgroundColor: "#DFCFBE", 
-                        color: "black",
-                        border: "1px solid #DFCFBE",
-                        height: "100%",
-                        width: "100%",
-                        padding: "4em",
-                        borderRadius: "25px",
-                        marginLeft: "1em",
-                    }}>
-                    <h2>Évenements JPO :</h2>
-                    <br/>
-                    {events.map((event) => (
-                        <div
-                            key={event.id} 
-                            className="grid grid-rows-2 grid-flow-col" 
-                            style={{
-                                backgroundColor: "#c9b6a1",
-                                border: "1px solid black",
-                                borderRadius: "25px",
-                                padding: "1em",
-                                marginBottom: "1em",
-                                cursor: "pointer",
-                                overflow: "scroll",
-                        }}>
-                            {event.title} <br/>
-                            {/* {event.creation_date}<br/> */}
-                            date/heure <br/>
-                            {event.description} 
-                        </div>
-                    ))}                      
-                </div>
+export default function Jpo() {
+  let navigate = useNavigate();
 
-                <div className="col-span-2 ..." 
-                    style={{
-                        backgroundColor: "#98B4D4",
-                        color: "black",
-                        border: "1px solid black",
-                        height: "100%",
-                        width: "90%",
-                        padding: "3em",
-                        borderRadius: "25px",
-                        marginLeft: "1em",
-                    }}>
-                    <h2>Explications JPO</h2>
-                    <p>Une journée portes ouvertes (JPO) est un événement durant lequel des monuments ou des lieux (historiques, industriels, …) d'accès généralement interdits au public sont exceptionnellement ouverts à la visite.
-                        Cette idée a reçu un écho durable avec la naissance, en France, en 1984, des « Journées portes ouvertes dans les monuments historiques », qui s'est ensuite développée dans l'ensemble de l'Europe pour en devenir les Journées européennes du patrimoine.
-                        Des journées portes ouvertes existent également dans d'autres contextes, par exemple pour visiter une école avant de s'y inscrire (participations à des cours, etc.) et pour mieux vendre ses produits dans le cadre d'une entreprise.
-                    </p>
-                </div>
+  const { user } = useFirebase();
+  const userStatus = user?.status;
 
-                <div className="grid grid-cols-4 gap-4" style={{
-                    width: "100%",
-                    marginLeft: "1em",
-                }}>
-                    <div style={{
-                        backgroundColor: "#939597",
-                        color: "black",
-                        border: "1px solid black",
-                        height: "100%",
-                        width: "250%",
-                        borderRadius: "25px",
-                        padding: "1em",
-                        cursor: "pointer"
-                    }} >
-                        <p>Projets qui peuvent être sélectionné pour être présenté en jpo</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  const [events, setEvent] = useState([]);
+  const [openProjects, setOpenProjects] = useState(false);
+
+  const handleClickProjects = () => {
+    setOpenProjects(!openProjects);
+  };
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      const response = await axios
+        .get("http://localhost:5050/ressources/jpo")
+        .then((response) => {
+          setEvent(response.data);
+        });
+    };
+    fetchEvent();
+  }, []);
+
+  const jpoData = {
+    jpoTitle: "JPO Name",
+    jpoDescription: "Je suis une JPO",
+    jpoThumbnail: "thumbnail.img",
+    jpoDayStart: "TimeStamp",
+    jpoDayEnd: "TimeStamp",
+    linkedStudentProject: [
+      {
+        nameProject: "Projet",
+        idStudentProject: "fbvofijdzeoiebvf",
+        typeProject: "Web",
+      },
+    ],
+    linkPlaquette: "imageplaquette.img",
+  };
+
+  return (
+    <div>
+      <h1>JPO</h1>
+      {userStatus === "pedago" ? (
+        <Button
+          variant="contained"
+          disableElevation
+          sx={{
+            marginLeft: "80%",
+          }}
+        >
+          Créer une JPO
+        </Button>
+      ) : (
+        <div></div>
+      )}
+
+      <div className="jpo-list-container">
+        <Card
+          className="jpo-card"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            width: "60%",
+          }}
+        >
+          <img
+            src={jpoData.jpoThumbnail}
+            alt="Je suis un JPO, c'est la description vous avez compris?"
+            className="jpo-image"
+          />
+          <div className="jpo-text">
+            <Typography sx={{ paddingBottom: "10px", fontSize: "24px" }}>
+              {jpoData.jpoTitle}
+            </Typography>
+            <Typography sx={{ paddingBottom: "10px" }}>
+              {jpoData.jpoDescription}
+            </Typography>
+            <Typography sx={{ textAlign: "center" }}>
+              {jpoData.jpoDayStart} {jpoData.jpoDayEnd}
+            </Typography>
+            {jpoData?.linkedStudentProject?.length > 0 ? (
+              <List>
+                <ListItem button onClick={handleClickProjects}>
+                  <ListItemText
+                    primary={"Afficher les projets"}
+                    style={{ textAlign: "center" }}
+                  />
+                  {openProjects ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={openProjects} timeout="auto" unmountOnExit>
+                  <List disablePadding>
+                    {jpoData.linkedStudentProject.map((project, index) => (
+                      <React.Fragment key={index}>
+                        <ListItem
+                          sx={{
+                            padding: "10px 0px",
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-evenly",
+                            borderTop: "1px solid grey",
+                          }}
+                        >
+                          <Typography>
+                            {project.nameProject}
+                            {" - "}
+                            {project.typeProject}
+                          </Typography>
+                          <Button
+                            onClick={() => {
+                              navigate(`/${project.idStudentProject}`);
+                            }}
+                          >
+                            Voir le projet
+                          </Button>
+                        </ListItem>
+                      </React.Fragment>
+                    ))}
+                  </List>
+                </Collapse>
+              </List>
+            ) : (
+              <div className="no-votes-student-projects-container">
+                <p className="no-votes-student-projects-p">
+                  Personne n'a encore mis en avant votre projet
+                </p>
+                {/* <img
+                  className="no-votes-student-projects-img"
+                  src={NoVotesImg}
+                  alt="no-votes-projects-students"
+                /> */}
+              </div>
+            )}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
