77e76bf955f727be6c568494681b98ba5c4f2900
Jerance <michael.jerance@gmail.com>
2023-06-01 12:47:52 +0200

    #94 feat/Add jpo controller in routes

--- server/jpoRoutes.js
+++ server/jpoRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllJpo, getJpoById, createJpo } = require("./controllers/jpo");
+const {
+  getAllJpo,
+  getJpoById,
+  createJpo,
+  linkProjectStudents,
+} = require("./controllers/jpo");
 
 module.exports = function () {
   // Routes Jpo
   router.get("/jpo", getAllJpo);
   router.get("/jpo/:id", getJpoById);
   router.post("/jpo", createJpo);
-  return router;
+  router.post("/jpo/:jpoId", linkProjectStudents);
   return router;
 };
