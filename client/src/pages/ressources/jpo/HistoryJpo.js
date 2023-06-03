dd4162abdaf5a47c10e67fba1a7660a88488f895
Jerance <michael.jerance@gmail.com>
2023-06-03 11:48:05 +0200

    #94 feat/Add skeletton for history jpo page

--- client/src/pages/ressources/jpo/HistoryJpo.js
+++ client/src/pages/ressources/jpo/HistoryJpo.js
@@ -14,6 +14,7 @@ import {
   CardMedia,
   Grid,
   Button,
+  Skeleton,
 } from "@mui/material";
 
 import ExpandLess from "@mui/icons-material/ExpandLess";
@@ -26,6 +27,8 @@ const HistoryJpo = () => {
   const [allPastJpo, setAllPastJpo] = useState([]);
 
   const [openProjects, setOpenProjects] = useState(false);
+
+  const [loading, setLoading] = useState(true);
 
   const handleClickProjects = () => {
     setOpenProjects(!openProjects);
@@ -47,104 +50,175 @@ const HistoryJpo = () => {
   };
 
   useEffect(() => {
-    getAllOldJpo();
+    getAllOldJpo()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
   }, []);
 
   return (
-    <div className="history-jpo-page">
-      <div className="header-jpo">
-        <Typography variant="h3" sx={{ fontWeight: "bold" }}>
-          Historique des JPO
-        </Typography>
-      </div>
-      <div className="jpo-list-container">
-        {allPastJpo.map((jpoData, index) => (
-          <Card
-            key={index}
-            className="jpo-card"
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              flexWrap: "wrap",
-              width: "70%",
-              margin: "30px",
-            }}
-          >
-            <Grid container>
-              <Grid item xs={12} sm={6}>
-                <CardMedia
-                  component="img"
-                  src={jpoData.jpoThumbnail}
-                  alt="img-jpo"
-                  className="jpo-image"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <div className="jpo-text">
-                  <Typography sx={{ paddingBottom: "10px", fontSize: "24px" }}>
-                    {jpoData.jpoTitle}
-                  </Typography>
-                  <Typography sx={{ paddingBottom: "10px" }}>
-                    {jpoData.jpoDescription}
-                  </Typography>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {moment
-                      .unix(jpoData.jpoDayStart._seconds)
-                      .format("DD.MM.YYYY HH:mm")}
-                    {" - "}
-                    {moment
-                      .unix(jpoData.jpoDayEnd._seconds)
-                      .format("DD.MM.YYYY HH:mm")}
-                  </Typography>
-                  {jpoData.linkedStudentProject ? (
-                    <List>
-                      <ListItem button onClick={handleClickProjects}>
-                        <ListItemText
-                          primary={"Afficher les projets"}
-                          style={{ textAlign: "center" }}
-                        />
-                        {openProjects ? <ExpandLess /> : <ExpandMore />}
-                      </ListItem>
-                      <Collapse in={openProjects} timeout="auto" unmountOnExit>
-                        <List disablePadding>
-                          <ListItem
-                            sx={{
-                              padding: "10px 0px",
-                              display: "flex",
-                              flexDirection: "row",
-                              justifyContent: "space-evenly",
-                              borderTop: "1px solid grey",
-                            }}
-                          >
-                            <Typography>
-                              {jpoData.linkedStudentProject.nameProject}
-                            </Typography>
-                            <Button
-                              onClick={() => {
-                                navigate(
-                                  `/studentprojects/${jpoData?.linkedStudentProject?.id}`
-                                );
-                              }}
+    <>
+      {loading ? (
+        <>
+          <div className="history-jpo-page">
+            <div className="header-jpo">
+              <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+                Historique des JPO
+              </Typography>
+            </div>
+            <div className="jpo-list-container">
+              {Array.from({ length: 3 }).map((_, index) => (
+                <Card
+                  key={index}
+                  className="jpo-card"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                    width: "70%",
+                    margin: "30px",
+                  }}
+                >
+                  <Grid container>
+                    <Grid item xs={12} sm={6}>
+                      <Skeleton width={400} height={400} />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <div className="jpo-text">
+                        <Typography
+                          sx={{ paddingBottom: "10px", fontSize: "24px" }}
+                        >
+                          <Skeleton width={200} />
+                        </Typography>
+                        <Typography sx={{ paddingBottom: "10px" }}>
+                          <Skeleton count={2} />
+                        </Typography>
+                        <Typography sx={{ textAlign: "center" }}>
+                          <Skeleton width={150} />
+                        </Typography>
+                        <div>
+                          <p>
+                            <Skeleton />
+                          </p>
+                        </div>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="history-jpo-page">
+            <div className="header-jpo">
+              <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+                Historique des JPO
+              </Typography>
+            </div>
+            <div className="jpo-list-container">
+              {allPastJpo.map((jpoData, index) => (
+                <Card
+                  key={index}
+                  className="jpo-card"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                    width: "70%",
+                    margin: "30px",
+                  }}
+                >
+                  <Grid container>
+                    <Grid item xs={12} sm={6}>
+                      <CardMedia
+                        component="img"
+                        src={jpoData.jpoThumbnail}
+                        alt="img-jpo"
+                        className="jpo-image"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <div className="jpo-text">
+                        <Typography
+                          sx={{ paddingBottom: "10px", fontSize: "24px" }}
+                        >
+                          {jpoData.jpoTitle}
+                        </Typography>
+                        <Typography sx={{ paddingBottom: "10px" }}>
+                          {jpoData.jpoDescription}
+                        </Typography>
+                        <Typography sx={{ textAlign: "center" }}>
+                          {moment
+                            .unix(jpoData.jpoDayStart._seconds)
+                            .format("DD.MM.YYYY HH:mm")}
+                          {" - "}
+                          {moment
+                            .unix(jpoData.jpoDayEnd._seconds)
+                            .format("DD.MM.YYYY HH:mm")}
+                        </Typography>
+                        {jpoData.linkedStudentProject ? (
+                          <List>
+                            <ListItem button onClick={handleClickProjects}>
+                              <ListItemText
+                                primary={"Afficher les projets"}
+                                style={{ textAlign: "center" }}
+                              />
+                              {openProjects ? <ExpandLess /> : <ExpandMore />}
+                            </ListItem>
+                            <Collapse
+                              in={openProjects}
+                              timeout="auto"
+                              unmountOnExit
                             >
-                              Voir le projet
-                            </Button>
-                          </ListItem>
-                        </List>
-                      </Collapse>
-                    </List>
-                  ) : (
-                    <div>
-                      <p>Personne n'a encore mis en avant votre projet</p>
-                    </div>
-                  )}
-                </div>
-              </Grid>
-            </Grid>
-          </Card>
-        ))}
-      </div>
-    </div>
+                              <List disablePadding>
+                                <ListItem
+                                  sx={{
+                                    padding: "10px 0px",
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    justifyContent: "space-evenly",
+                                    borderTop: "1px solid grey",
+                                  }}
+                                >
+                                  <Typography>
+                                    {jpoData.linkedStudentProject.nameProject}
+                                  </Typography>
+                                  <Button
+                                    onClick={() => {
+                                      navigate(
+                                        `/studentprojects/${jpoData?.linkedStudentProject?.id}`
+                                      );
+                                    }}
+                                  >
+                                    Voir le projet
+                                  </Button>
+                                </ListItem>
+                              </List>
+                            </Collapse>
+                          </List>
+                        ) : (
+                          <div>
+                            <p>Personne n'a encore mis en avant votre projet</p>
+                          </div>
+                        )}
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
--- client/src/pages/ressources/students_project/StudentProjectInfo.js
+++ client/src/pages/ressources/students_project/StudentProjectInfo.js
@@ -592,7 +592,7 @@ const StudentProjectInfo = () => {
               allblogtutos={allblogtutos}
               getStudentProjectById={getStudentProjectById}
             />
-          </div>{" "}
+          </div>
         </>
       )}
     </>
