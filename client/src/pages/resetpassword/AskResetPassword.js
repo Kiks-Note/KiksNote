b6be416d723e8335c90914d928b6164208df1c04
Jerance <michael.jerance@gmail.com>
2023-06-07 23:10:03 +0200

    #108 fix/Refont ask reset password page

--- client/src/pages/resetpassword/AskResetPassword.js
+++ client/src/pages/resetpassword/AskResetPassword.js
@@ -1,81 +1,151 @@
+import React, { useState } from "react";
+import "./AskResetPassword.scss";
+import axios from "axios";
+import { Box, TextField, Button, Container, Typography } from "@mui/material";
 
-import React, { useRef, useState } from 'react'
-import "./AskResetPassword.scss"
-import Divider from '@mui/material/Divider';
-import axios from 'axios';
-import { Box, TextField, Button } from "@mui/material";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useTheme } from "@mui/material";
 
-import {
-    useTheme,
-} from "@mui/material";
-
+import ForgotPasswordArt from "../../assets/img/forgot_password.svg";
 
 const options = {
-    autoClose: 2000,
-    className: '',
-    position: toast.POSITION.TOP_RIGHT,
+  autoClose: 2000,
+  className: "",
+  position: toast.POSITION.TOP_RIGHT,
+  theme: "colored",
 };
 
-export const toastSuccess = message => {
-    toast.success(message, options);
-}
+export const toastSuccess = (message) => {
+  toast.success(message, options);
+};
 
-export const toastFail = message => {
-    toast.error(message, options);
-}
+export const toastFail = (message) => {
+  toast.error(message, options);
+};
 
-function AskResetPassword() {
+const AskResetPassword = () => {
+  const theme = useTheme();
+  const [mail, setMail] = useState("");
 
-    const theme = useTheme();
-    const [mail, setMail] = useState('');
-
-    const sendEmailFromFront = async () => {
-        try {
-            await axios.post("http://localhost:5050/auth/sendemail", {
-                email: mail
-            }).then((response) => {
-                console.log(response.data)
-                if (response.status === 200) {
-                    toastSuccess("Mail envoyé");
-                } else {
-                    toastFail("Mail non valide");
-                }
-            }).catch((err) => {
-                console.log(err);
-            });
-        } catch (error) {
-            console.log(error)
-        }
-    };
-
-    const form = useRef();
-
-    function handleSubmit(e) {
-        sendEmailFromFront();
-        e.preventDefault();
-        form.current.reset();
+  const sendEmailFromFront = async () => {
+    try {
+      await axios
+        .post("http://localhost:5050/auth/sendemail", {
+          email: mail,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            toastSuccess(`Mail envoyé à ${mail}`);
+          }
+        })
+        .catch((err) => {
+          if (err.response.status === 404) {
+            toastFail(err.response.data.message);
+          }
+        });
+    } catch (error) {
+      toastFail(error.response.data.message);
     }
+  };
 
-    return (
+  const onChangeEmail = (e) => {
+    e.preventDefault();
+    setMail(e.target.value);
+  };
 
-        <div className="form-reset-with-mail-container" style={{ backgroundColor: theme.palette.background.paper }}>
-            <Box sx={{ boxShadow: 5 }} style={{ width: "50%", padding: "15px", backgroundColor: theme.palette.background.container }} className="box-form-reset-with-mail" >
-                <h1 style={{ margin: 10, fontSize: "2vw" }}>Réinitialiser le mot de passe</h1>
-                <Divider variant="middle" style={{ background: "#FFF000", height: "1px", width: "50%" }} className="divider-form-reset-with-mail" />
-                <form className='form-ask-reset' ref={form} onSubmit={(e) => handleSubmit(e)} style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <label>Email</label>
-                    <TextField placeholder="Mail" required id="my-mail-id" type="email" name="user_email" onInput={e => setMail(e.target.value)} sx={{
-                        input: { color: 'text.primary' }
-                    }} />
-                    <Button id='submit-email' type="submit" variant="contained" style={{ backgroundColor: "#fff", color: "black", fontWeight: "bold", textTransform: 'none', margin: 10, marginTop: "10%" }}>Envoyer</Button>
-                </form>
-            </Box>
-            <ToastContainer></ToastContainer>
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendEmailFromFront();
+  };
 
-    );
-}
-export default AskResetPassword; 
+  return (
+    <div
+      className="reset-password-page"
+      style={{ backgroundColor: theme.palette.background.paper }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          style={{
+            width: "80%",
+            height: "70%",
+            display: "flex",
+            padding: "20px",
+            backgroundColor: theme.palette.background.container,
+          }}
+          className="box-form-reset-with-mail"
+        >
+          <Typography
+            variant="h3"
+            fontWeight="bold"
+            style={{ margin: 10, fontSize: "2vw" }}
+          >
+            Réinitialiser le mot de passe
+          </Typography>
+          <div style={{ padding: "5%" }}>
+            <img
+              src={ForgotPasswordArt}
+              style={{
+                width: "100%",
+                borderRadius: "25px",
+                objectFit: "scale-down",
+                backgroundColor: "#7a52e1",
+              }}
+              alt="forgot-password-img"
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <TextField
+              placeholder="votre adresse mail"
+              variant="standard"
+              fullWidth
+              className="textfield-login"
+              label="Adresse mail"
+              id="input-email"
+              defaultValue={mail}
+              onChange={onChangeEmail}
+              sx={{
+                input: { color: "text.primary" },
+                width: "70%",
+              }}
+            />
+            <Button
+              id="submit-email"
+              type="submit"
+              variant="contained"
+              style={{
+                backgroundColor: "#7a52e1",
+                color: "black",
+                fontWeight: "bold",
+                textTransform: "none",
+                margin: "10px",
+                marginTop: "20px",
+              }}
+              onClick={handleSubmit}
+            >
+              Envoyer
+            </Button>
+          </div>
+        </Box>
+      </div>
+
+      <ToastContainer></ToastContainer>
+    </div>
+  );
+};
+export default AskResetPassword;
