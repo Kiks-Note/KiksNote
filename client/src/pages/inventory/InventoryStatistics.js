71db861764ccf5319d2cc18331801a92ea2c8770
Alan <91255496+AlanSann@users.noreply.github.com>
2023-06-12 15:36:49 +0200

    #55 feat/inventory/stats improving my doughnut function to make it look better and perform better, its now displaying labels of materials aswell

--- client/src/pages/inventory/InventoryStatistics.js
+++ client/src/pages/inventory/InventoryStatistics.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Chart, CategoryScale, LinearScale, Title, TimeScale } from 'chart.js';
+import { Chart, CategoryScale, LinearScale, Title, TimeScale, Tooltip, LineElement, Legend, PointElement, Filler } from 'chart.js';
 import { BarController } from 'chart.js';
 import { BarElement } from 'chart.js';
 import { DoughnutController, ArcElement } from 'chart.js';
 import moment from 'moment';
 import 'chartjs-adapter-moment';
+import 'chartjs-plugin-datalabels';
+import { Doughnut } from 'react-chartjs-2';
+import { set } from 'date-fns';
 
 function InventoryStatistics() {
     const chartRef = useRef(null);
@@ -136,86 +139,30 @@ function InventoryStatistics() {
 
 function MostUsedMaterials() {
     const chartRef = useRef(null);
+    const [data, setData] = useState([]);
+    const [chartInstance, setChartInstance] = useState(null);
+
 
     useEffect(() => {
-        Chart.register(DoughnutController, ArcElement);
+        Chart.register(
+            ArcElement,
+            Title,
+            LineElement,
+            CategoryScale,
+            LinearScale,
+            PointElement,
+            Filler,
+            Tooltip,
+            Legend
+        );
 
         const fetchData = async () => {
             try {
                 const response = await fetch('http://localhost:5050/inventory/statistics2');
                 console.log('Réponse du serveur:', response);
                 const data = await response.json();
-
-                // Filtrer les données pour n'inclure que les matériels "accepted"
-                const acceptedMaterials = data.filter(document => document.status === 'accepted');
-
-                // Traiter les données pour obtenir le top 5 des matériels les plus utilisés
-                const topMaterials = acceptedMaterials.reduce((acc, curr) => {
-                    const materialId = curr.deviceId;
-                    acc[materialId] = (acc[materialId] || 0) + 1;
-                    return acc;
-                }, {});
-
-                // Convertir le topMaterials en tableau pour le tri
-                const topMaterialsArray = Object.entries(topMaterials).map(([deviceId, count]) => ({ deviceId, count }));
-
-                // Trier le tableau en fonction du nombre d'utilisations (count) de manière décroissante
-                topMaterialsArray.sort((a, b) => b.count - a.count);
-
-                // Obtenir uniquement les 5 premiers matériels
-                const top5Materials = topMaterialsArray.slice(0, 5);
-
-                // Extraire les labels et les valeurs pour le graphique
-                const labels = top5Materials.map(document => document.deviceId);
-                const values = top5Materials.map(document => document.count);
-
-                const chartData = {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: 'Les matériels les plus utilisés',
-                            data: values,
-                            backgroundColor: [
-                                'rgba(255, 99, 132)',
-                                'rgba(255, 159, 64)',
-                                'rgba(255, 205, 86)',
-                                'rgba(75, 192, 192)',
-                                'rgba(54, 162, 235)',
-                            ],
-                            hoverOffset: 4,
-                        },
-                    ],
-                };
-
-                const options = {
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: 'Matériels les plus utilisés',
-                            position: 'top',
-                            align: 'center',
-                            font: {
-                                size: 18,
-                                weight: 'bold',
-                            },
-                        },
-                        legend: {
-                            display: true,
-                            position: 'right',
-                        },
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                };
-
-                if (chartRef.current) {
-                    const ctx = chartRef.current.getContext('2d');
-                    new Chart(ctx, {
-                        type: 'doughnut',
-                        data: chartData,
-                        options: options,
-                    });
-                }
+                const formattedData = data.map((document) => ({ ...document, count: moment(document.deviceId) }));
+                setData(formattedData);
             } catch (error) {
                 console.error(error);
             }
@@ -223,6 +170,98 @@ function MostUsedMaterials() {
 
         fetchData();
     }, []);
+
+    useEffect(() => {
+        if (data && data.length > 0) {
+            const labels = [];
+            const values = [];
+            // Filtrer les données pour n'inclure que les matériels "accepted"
+            const acceptedMaterials = data.filter(document => document.status === 'accepted');
+
+            // Traiter les données pour obtenir le top 5 des matériels les plus utilisés
+            const topMaterials = acceptedMaterials.reduce((acc, curr) => {
+                const materialId = curr.deviceId;
+                acc[materialId] = (acc[materialId] || 0) + 1;
+                return acc;
+            }, {});
+
+            // Convertir le topMaterials en tableau pour le tri
+            const topMaterialsArray = Object.entries(topMaterials).map(([deviceId, count]) => ({ deviceId, count }));
+
+            // Trier le tableau en fonction du nombre d'utilisations (count) de manière décroissante
+            topMaterialsArray.sort((a, b) => b.count - a.count);
+
+            // Obtenir uniquement les 5 premiers matériels
+            const top5Materials = topMaterialsArray.slice(0, 5);
+
+            // Extraire les labels et les valeurs pour le graphique
+            top5Materials.forEach((document) => {
+                labels.push(document.deviceId);
+                values.push(document.count);
+            });
+            console.log('labels:', labels);
+
+            const chartData = {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Les matériels les plus utilisés',
+                        data: values,
+                        backgroundColor: [
+                            'rgba(255, 99, 132)',
+                            'rgba(255, 159, 64)',
+                            'rgba(255, 205, 86)',
+                            'rgba(75, 192, 192)',
+                            'rgba(54, 162, 235)',
+                        ],
+                        hoverOffset: 4,
+                    },
+                ],
+            };
+
+            console.log('chartData:', chartData);
+
+            const options = {
+                type: 'doughnut',
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Matériels les plus utilisés',
+                        position: 'top',
+                        align: 'center',
+                        font: {
+                            size: 18,
+                            weight: 'bold',
+                        },
+                    },
+                    legend: {
+                        display: true,
+                        position: 'right',
+                        labels: {
+                            display: true,
+                            color: 'white',
+                        },
+                    },
+                },
+            };
+
+            if (chartInstance) {
+                chartInstance.destroy();
+            }
+
+            if (chartRef.current) {
+                const ctx = chartRef.current.getContext('2d');
+                const newChartInstance = new Chart(ctx, {
+                    type: 'doughnut',
+                    data: chartData,
+                    options: options,
+                });
+                setChartInstance(newChartInstance);
+            }
+        }
+    }, [data]);
 
     return (
         <div style={{ width: '600px', height: '330px' }}>
@@ -260,7 +299,6 @@ function TotalMacCount() {
 
     return <div>Nombre total de Mac : <span >{macCount}</span></div>;
 }
-
 
 
 function DashboardStats() {
